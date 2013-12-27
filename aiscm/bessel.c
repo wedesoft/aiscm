f06b237343aff57324b5273436db34ec74073d68
Jan Wedekind <jan@wedesoft.de>
2013-12-27 22:13:10 +0000

    Trying to implement "malloc"

--- aiscm/bessel.c
+++ aiscm/bessel.c
@@ -1,12 +1,19 @@
 #include <math.h>
 #include <libguile.h>
 
-SCM j0_wrapper (SCM x)
+SCM j0_wrapper(SCM x)
 {
-  return scm_from_double (j0 (scm_to_double (x)));
+  return scm_from_double(j0(scm_to_double(x)));
 }
 
-void init_bessel ()
+SCM malloc_wrapper(SCM x)
 {
-  scm_c_define_gsubr ("j0", 1, 0, 0, j0_wrapper);
+  void *mem = scm_gc_malloc_pointerless(scm_to_int(x), "aiscm");
+  return scm_from_pointer(mem, NULL);
+}
+
+void init_bessel(void)
+{
+  scm_c_define_gsubr("j0", 1, 0, 0, j0_wrapper);
+  scm_c_define_gsubr("malloc", 1, 0, 0, malloc_wrapper);
 }
--- aiscm/bessel.scm
+++ aiscm/bessel.scm
@@ -1,3 +1,7 @@
 (define-module (math bessel)
-  #:export (j0))
-(load-extension "libguile-bessel" "init_bessel")
+  #:export (j0
+            malloc))
+;(use-modules (rnrs bytevectors))
+(load-extension "guile-bessel" "init_bessel")
+;(define (malloc size)
+;  (make-bytevector size))
