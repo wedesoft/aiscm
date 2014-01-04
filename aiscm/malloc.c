930c669d3173a686a6dfe494cbc2cbee3afc6e22
Jan Wedekind <jan@wedesoft.de>
2014-01-04 13:56:19 +0000

    Using GOOPS OO-library

--- aiscm/malloc.c
+++ aiscm/malloc.c
@@ -1,32 +1,13 @@
 #include <libguile.h>
 
-SCM make_malloc_wrapper(SCM size)
+SCM gc_malloc_pointerless_wrapper(SCM size)
 {
   void *mem = scm_gc_malloc_pointerless(scm_to_int(size), "aiscm");
   SCM ptr = scm_from_pointer(mem, NULL);
-  return scm_cons(ptr, scm_cons(ptr, scm_cons(size, SCM_EOL)));
-}
-
-SCM malloc_plus_wrapper(SCM malloc, SCM offset)
-{
-  void *mem = SCM_POINTER_VALUE(scm_car(malloc));
-  int size = scm_to_int(scm_caddr(malloc));
-  int diff = scm_to_int(offset);
-  if (diff < 0)
-    scm_throw(scm_str2symbol("malloc-plus-offset-lt-zero"),
-              scm_list_1(offset));
-  if (diff > size)
-    scm_throw(scm_str2symbol("malloc-plus-offset-gt-size"),
-              scm_list_2(offset, scm_caddr(malloc)));
-  SCM ptr = scm_from_pointer(mem + diff, NULL);
-  return scm_cons(ptr,
-                  scm_cons(scm_cadr(malloc),
-                           scm_cons(scm_from_int(size - diff),
-                                    SCM_EOL)));
+  return ptr;
 }
 
 void init_malloc(void)
 {
-  scm_c_define_gsubr("make-malloc", 1, 0, 0, make_malloc_wrapper);
-  scm_c_define_gsubr("malloc-plus", 2, 0, 0, malloc_plus_wrapper);
+  scm_c_define_gsubr("gc-malloc-pointerless", 1, 0, 0, gc_malloc_pointerless_wrapper);
 }
--- aiscm/malloc.scm
+++ aiscm/malloc.scm
@@ -1,20 +1,45 @@
 (define-module (aiscm malloc)
+  #:use-module (oop goops)
   #:use-module (system foreign)
   #:use-module (rnrs bytevectors)
   #:export (make-malloc
-            malloc-plus
-            malloc-read
-            malloc-write))
+            get-memory
+            get-size
+            plus
+            read
+            write))
+;            malloc-plus
+;            malloc-read
+;            malloc-write))
 (load-extension "libguile-malloc" "init_malloc")
-(define (malloc-read malloc size)
+(define-class <malloc> ()
+  (memory #:init-keyword #:memory #:getter get-memory)
+  (base #:init-keyword #:base)
+  (size #:init-value 0 #:init-keyword #:size #:getter get-size))
+(define (make-malloc size)
+  (let ((ptr (gc-malloc-pointerless size)))
+    (make <malloc> #:memory ptr #:base ptr #:size size)))
+(define-method (plus (self <malloc>) offset)
+  (let ((size (get-size self)))
+    (cond
+      ((< offset 0) (throw 'malloc-plus-offset-lt-zero offset))
+      ((> offset size) (throw 'malloc-plus-offset-gt-size offset size))
+      (else
+        (make <malloc>
+          #:memory (make-pointer (+ offset (pointer-address (get-memory self))))
+          #:base (slot-ref self 'base)
+          #:size (- size offset))))))
+(define-generic read)
+(define-method (read (self <malloc>) size)
   (if
-    (> size (caddr malloc))
-    (throw 'malloc-read-size-overrun size (caddr malloc))
-    (pointer->bytevector (car malloc) size)))
-(define (malloc-write malloc bv)
+    (> size (get-size self))
+    (throw 'malloc-read-size-overrun size (get-size self))
+    (pointer->bytevector (get-memory self) size)))
+(define-generic write)
+(define-method (write (self <malloc>) bv)
   (begin
     (bytevector-copy!
       bv 0
-      (pointer->bytevector (car malloc) (caddr malloc)) 0
+      (pointer->bytevector (get-memory self) (get-size self)) 0
       (bytevector-length bv))
     bv))
