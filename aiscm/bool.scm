78a23e5f4db1e76620b32e8579a355fcaae726b4
Jan Wedekind <jan@wedesoft.de>
2014-01-09 20:57:50 +0000

    Changed 'unpack-bool' to 'unpack' with parameter

--- aiscm/bool.scm
+++ aiscm/bool.scm
@@ -3,15 +3,17 @@
   #:use-module (oop goops)
   #:use-module (rnrs bytevectors)
   #:export (<bool>
+            <meta<bool>>
             make-bool
             pack
-            unpack-bool))
-(define-class <bool> (<element>))
+            unpack))
+(define-class <meta<bool>> (<class>))
+(define-class <bool> (<element>) #:metaclass <meta<bool>>)
 (define (make-bool value)
   (make <bool> #:value value))
 (define-method (equal? (a <bool>) (b <bool>))
   (equal? (slot-ref a 'value) (slot-ref b 'value)))
 (define-method (pack (self <bool>))
   (u8-list->bytevector (list (if (slot-ref self 'value) 1 0))))
-(define (unpack-bool packed)
+(define-method (unpack (self <meta<bool>>) (packed <bytevector>))
   (make-bool (if (eq? (car (bytevector->u8-list packed)) 0) #f #t)))
