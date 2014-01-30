11a01e38a5c487652781e22ee9ade590f71a9351
Jan Wedekind <jan@wedesoft.de>
2014-01-30 17:54:31 +0000

    Using register objects instead of symbols

--- aiscm/jit.scm
+++ aiscm/jit.scm
@@ -9,7 +9,15 @@
             ADD
             MOV
             NOP
-            RET))
+            RET
+            EAX
+            ECX
+            EDX
+            EBX
+            ESP
+            EBP
+            ESI
+            EDI))
 ; http://www.drpaulcarter.com/pcasm/
 ; http://www.intel.com/content/www/us/en/processors/architectures-software-developer-manuals.html
 (load-extension "libguile-jit" "init_jit")
@@ -20,45 +28,41 @@
 (define ADD_r/m32,imm32 #x81)
 (define MOV_r/m32,r32   #x89)
 (define MOV_r32,imm32   #xb8)
-(define (opcode op)
-  (assq-ref opcodes op))
-(define register-codes
-  '((EAX . 0)
-    (ECX . 1)
-    (EDX . 2)
-    (EBX . 3)
-    (ESP . 4)
-    (EBP . 5)
-    (ESI . 6)
-    (EDI . 7)))
-(define (register-code reg)
-  (assq-ref register-codes reg))
+(define-class <register> () (code #:init-keyword #:code #:getter get-code))
+(define EAX (make <register> #:code 0))
+(define ECX (make <register> #:code 1))
+(define EDX (make <register> #:code 2))
+(define EBX (make <register> #:code 3))
+(define ESP (make <register> #:code 4))
+(define EBP (make <register> #:code 5))
+(define ESI (make <register> #:code 6))
+(define EDI (make <register> #:code 7))
 (define (ModR/M mod reg/opcode r/m)
   (list (logior (ash mod 6) (ash reg/opcode 3) r/m)))
-(define-method (ADD (r/m32 <symbol>) (r32 <symbol>))
+(define-method (ADD (r/m32 <register>) (r32 <register>))
   (let ((op ADD_r/m32,r32)
-        (reg (register-code r32))
-        (r/m (register-code r/m32))
+        (reg (get-code r32))
+        (r/m (get-code r/m32))
         (mod #b11))
     (append (list op) (ModR/M mod reg r/m))))
-(define-method (ADD (r/m32 <symbol>) (imm32 <int>))
+(define-method (ADD (r/m32 <register>) (imm32 <int>))
   (let ((id (bytevector->u8-list (pack imm32))))
-    (if (eq? r/m32 'EAX)
+    (if (equal? r/m32 EAX)
       (let ((op ADD_EAX,imm32))
         (append (list op) id))
       (let ((op ADD_r/m32,imm32)
-            (r/m (register-code r/m32))
+            (r/m (get-code r/m32))
             (mod #b11))
         (append (list op) (ModR/M mod 0 r/m) id)))))
-(define-method (MOV (r/m32 <symbol>) (r32 <symbol>))
+(define-method (MOV (r/m32 <register>) (r32 <register>))
   (let ((op MOV_r/m32,r32)
-        (reg (register-code r32))
-        (r/m (register-code r/m32))
+        (reg (get-code r32))
+        (r/m (get-code r/m32))
         (mod #b11))
     (append (list op) (ModR/M mod reg r/m))))
-(define-method (MOV (r32 <symbol>) (imm32 <int>))
+(define-method (MOV (r32 <register>) (imm32 <int>))
   (let ((op  MOV_r32,imm32)
-        (reg (register-code r32))
+        (reg (get-code r32))
         (id  (bytevector->u8-list (pack imm32))))
     (append (list (logior op reg)) id)))
 (define (NOP) '(#x90))
