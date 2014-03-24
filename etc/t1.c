2ad631ef8c044373027410ddd33d7b5eaa8e7bc2
Jan Wedekind (Dr) <jan@wedesoft.de>
2014-03-24 18:54:41 +0000

    Changed base class of <address>

--- etc/t1.c
+++ etc/t1.c
@@ -1,17 +1,7 @@
-void test(int *r, char *x, int n)
+int test(char *p, short *q, int *r)
 {
-  char *p = x;
-  int *q = r;
-  int *qend = r + n;
-  while (q != qend) {
-    char  *x1 = (char  *)q;
-    short *x2 = (short *)q;
-    int   *x3 = (int   *)q;
-    int c = *p;
-    *x1 = (char)c;
-    *x2 = (short)c;
-    *x3 = c;
-    p++;
-    q++;
-  };
+  int a = *p;
+  int b = *q;
+  int c = *r;
+  return a + b + c;
 }
--- etc/t2.c
+++ etc/t2.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 
-void test(int *r, char *x, int n);
+int test(char *p, short *q, int *r);
 
 int main(int argc, long *argv[])
 {
   int n = 3;
-  char *x = (char *)malloc(n * sizeof(char));
-  int *r = (int *)malloc(n * sizeof(int));
-  test(r, x, n);
+  int *x = (int *)malloc(n * sizeof(int));
+  test((char *)x, (short *)x, (int *)x);
   free(x);
-  free(r);
   return 0;
 }
