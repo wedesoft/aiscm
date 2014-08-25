dacd82ad7c08c84b8dfb170c5f630c8a1e9bc0bd
Jan Wedekind (Dr) <jan@wedesoft.de>
2014-08-25 22:04:42 +0100

    Working on V4L2 input

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 # TODO
 
+* V4L2
 * implement infinite register machine (register allocator)
-* PPM I/O
+  nur argumente evaluieren (e.g. (MOV rl (get-value r_)) -> (MOV (reg:LI 2) (arg 5)))
 * (lambda (fun ...) (env fun [(r (reg ... fun)) ...] ...)) ->
   (env (r_ a_) [(r (reg ...) ...] ...))
 * print arrays
 * getting ranges from array
+* use bytevectors? http://www.gnu.org/software/guile/manual/html_node/Dynamic-FFI.html
--- aiscm/v4l2.c
+++ aiscm/v4l2.c
@@ -9,6 +9,8 @@ static scm_t_bits v4l2_tag;
 
 struct v4l2_t {
   int fd;
+  struct v4l2_format format;
+  struct v4l2_requestbuffers req;
 };
 
 static int xioctl(int fd, int request, void *arg)
@@ -39,7 +41,7 @@ SCM close_v4l2(SCM self)
 SCM make_v4l2(SCM device_name, SCM channel)
 {
   SCM retval;
-  struct v4l2_t *v4l2;
+  struct v4l2_t *self;
   struct stat st;
   int fd;
   const char *device_c_str = scm_to_locale_string(device_name);
@@ -47,15 +49,15 @@ SCM make_v4l2(SCM device_name, SCM channel)
   if (!S_ISCHR(st.st_mode)) scm_misc_error("make_v4l2", "'~a' is not a device", scm_list_1(device_name));
   fd = open(device_c_str, O_RDWR, 0);
   if (fd == -1) scm_syserror("make_v4l2");
-  v4l2 = (struct v4l2_t *)scm_gc_malloc(sizeof(struct v4l2_t), "v4l2");
-  SCM_NEWSMOB(retval, v4l2_tag, v4l2);
-  v4l2->fd = fd;
-  struct v4l2_capability capability;
-  if (xioctl(fd, VIDIOC_QUERYCAP, &capability)) {
+  self = (struct v4l2_t *)scm_gc_malloc(sizeof(struct v4l2_t), "v4l2");
+  SCM_NEWSMOB(retval, v4l2_tag, self);
+  self->fd = fd;
+  struct v4l2_capability cap;
+  if (xioctl(fd, VIDIOC_QUERYCAP, &cap)) {
     close_v4l2(retval);
     scm_syserror("make_v4l2");
   };
-  if (capability.capabilities & V4L2_CAP_VIDEO_CAPTURE == 0) {
+  if (cap.capabilities & V4L2_CAP_VIDEO_CAPTURE == 0) {
     close_v4l2(retval);
     scm_misc_error("make_v4l2", "'~a' is not a video capture device", scm_list_1(device_name));
   };
@@ -64,6 +66,38 @@ SCM make_v4l2(SCM device_name, SCM channel)
     close_v4l2(retval);
     scm_misc_error("make_v4l2", "Error selecting channel ~a for device '~a'",
                    scm_list_2(channel, device_name));
+  };
+  // TODO: implement selection of video mode
+  self->format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  self->format.fmt.pix.width = 640;
+  self->format.fmt.pix.height = 480;
+  self->format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+  self->format.fmt.pix.field = V4L2_FIELD_ANY;
+  if (xioctl(fd, VIDIOC_S_FMT, &self->format)) {
+    close_v4l2(retval);
+    scm_misc_error("make_v4l2", "Error switching device '~a' to selected format",
+                   scm_list_1(device_name));
+  };
+  if (cap.capabilities & V4L2_CAP_STREAMING) {
+    memset(&self->req, 0, sizeof(self->req));
+    self->req.count = 2;
+    self->req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    self->req.memory = V4L2_MEMORY_MMAP;
+    if (xioctl(fd, VIDIOC_REQBUFS, &self->req)) {
+      // TODO: Try user pointer I/O
+      close_v4l2(retval);
+      scm_misc_error("make_v4l2", "Memory mapped I/O not supported for device '~a'",
+                     scm_list_1(device_name));
+    };
+    if (self->req.count < 2) {
+      close_v4l2(retval);
+      scm_misc_error("make_v4l2", "Insufficient buffer memory on device '~a'",
+                     scm_list_1(device_name));
+    };
+  } else {
+    close_v4l2(retval);
+    scm_misc_error("make_v4l2", "Device '~a' neither supports streaming nor reading from device",
+                   scm_list_1(device_name));
   };
   return retval;
 }
