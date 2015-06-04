5fde7ca65cb23d91f831b897145ba3f552ed2f58
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-06-04 23:10:31 +0100

    Simpler test suite

--- tests/guile-tap.scm
+++ tests/guile-tap.scm
@@ -7,23 +7,15 @@
 (define test-counter 0)
 (define (incr-counter) (set! test-counter (+ test-counter 1)))
 
-(define (ok result . description)
+(define (ok result description)
   (incr-counter)
-  (format #t "~a ~a" (if result "ok" "not ok") test-counter)
-  (if (not (null? description)) (format #t " - ~a" (car description)))
-  (format #t "~&~!"))
+  (format #t "~a ~a - ~a~&~!" (if result "ok" "not ok") test-counter description))
 
-(define-syntax-rule (skip expr . explanation)
-  (if (not (null? (quote explanation)))
-    (ok #t (string-append (car (quote explanation)) " # SKIP"))
-    (ok #t "# SKIP")))
+(define-syntax-rule (skip expr description)
+  (ok #t (string-append description " # SKIP")))
 
-(define-syntax-rule (todo expr . explanation)
-  (if (not (null? (quote explanation)))
-    (ok expr (string-append (car (quote explanation)) " # TODO"))
-    (ok expr "# TODO")))
-
-(define (diagnostic message) (format #t "# ~a~&~!" message))
+(define (todo expr description)
+  (ok expr (string-append description " # TODO")))
 
 (define-syntax-rule (throws? expr)
   (catch #t
