d380839d108514fc67fc64df8d441dd13ff2a1a3
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-07-14 17:17:29 +0100

    Matching and foreign interface for floating point

--- aiscm/float.scm
+++ aiscm/float.scm
@@ -1,21 +1,22 @@
 (define-module (aiscm float)
   #:use-module (oop goops)
+  #:use-module (system foreign)
   #:use-module (rnrs bytevectors)
   #:use-module (aiscm util)
   #:use-module (aiscm element)
-  #:export (float single double precision double?
+  #:export (floating-point single-precision double-precision precision double?
             <float<>> <meta<float<>>>
             <float>  <float<single>> <meta<float<single>>>
             <double> <float<double>> <meta<float<double>>>))
-(define single 'single)
-(define double 'double)
+(define single-precision 'single)
+(define double-precision 'double)
 (define-class <meta<float<>>> (<meta<element>>))
 (define-class <float<>> (<element>)
               #:metaclass <meta<float<>>>)
 (define-method (write (self <float<>>) port)
   (format port "#<~a ~a>" (class-name (class-of self)) (get-value self)))
 (define-generic precision)
-(define (float prec)
+(define (floating-point prec)
   (let* [(name      (format #f "<float<~a>>" prec))
          (metaname  (format #f "<meta~a>" name))
          (metaclass (def-once metaname (make <class>
@@ -26,9 +27,11 @@
                                          #:name name)))]
     (define-method (precision (self metaclass)) prec)
     retval))
-(define <float>  (float single))
-(define <double> (float double))
-(define (double? self) (eq? double (precision self)))
+(define <float>  (floating-point single-precision))
+(define <double> (floating-point double-precision))
+(define-method (foreign-type (t  <meta<float<single>>>)) float)
+(define-method (foreign-type (t  <meta<float<double>>>)) double)
+(define (double? self) (eq? double-precision (precision self)))
 (define-method (size-of (self <meta<float<single>>>)) 4)
 (define-method (size-of (self <meta<float<double>>>)) 8)
 (define-method (pack (self <float<>>))
@@ -43,5 +46,6 @@
   (let* [(ref   (if (double? self) bytevector-ieee-double-native-ref bytevector-ieee-single-native-ref))
          (value (ref packed 0))]
     (make self #:value value)))
+(define-method (match (i <real>) . args) <double>)
 (define-method (types (self <meta<float<>>>)) (list self))
 (define-method (content (self <real>)) (list self))
