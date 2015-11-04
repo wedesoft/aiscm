81ad89f678f1f7fe5b8bb94fc94e6eccfb235553
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-11-04 21:40:33 +0000

    Moved etc/test.scm code into etc/env.scm

--- etc/env.scm
+++ etc/env.scm
@@ -56,20 +56,71 @@
 (test x)
 
 
+(use-modules (ice-9 format))
+(use-modules (oop goops))
+;(use-modules (aiscm element))
+;(use-modules (aiscm int))
+;(use-modules (aiscm jit))
+;(use-modules (system foreign))
+;(define c (make <context>))
 
-(define-method (to-type (target <meta<int<>>>) (self <sequence<>>))
-  (let [(proc
-          (if (>= (size-of (typecode self)) (size-of target))
-              (let [(ratio (/ (size-of (typecode self)) (size-of target)))]
-                (lambda (target self)
-                  (make (to-type target (class-of self))
-                        #:shape (shape self)
-                        #:strides (map (cut * ratio <>) (strides self))
-                        #:value (get-value self))))
-              (let [(fun (jit ctx (list (class-of self)) (cut to-type target <>)))]
-                (lambda (target self) (fun self)))))]
-    (add-method! to-type
-                 (make <method>
-                       #:specializers (list (class-of target) (class-of self))
-                       #:procedure proc))
-    (to-type target self)))
+;(define-method (+ (a <int>) (b <int>))
+;  (let ((f (asm c int (list (MOV EAX EDI) (ADD EAX ESI) (RET)) int int)))
+;    (make <int> #:value (f (get-value a) (get-value b)))))
+
+;(define i (make <int> #:value 42))
+;(define j (make <int> #:value 13))
+
+;(format #t "~a + ~a = ~a~&" (get-value i) (get-value j) (get-value (+ i j)))
+;(format #t "~a + ~a = ~a~&" (get-value j) (get-value i) (get-value (+ j i)))
+;(format #t "Compiled ~a method(s).~&" (length (slot-ref c 'binaries)))
+
+(define (expr->descr expr)
+  (letrec
+    ((expr->str
+       (lambda (expr)
+         (cond
+           ((null? expr) "")
+           ((pair? expr) (format #f "<~a_~a>"
+                                 (car expr)
+                                 (string-join (map expr->str (cdr expr)) "_")))
+           (else "?")))))
+    (format #t "~s~&" expr)
+    (string->symbol (expr->str expr))))
+
+(define (expr->params expr)
+  (cond
+    ((null? expr) '())
+    ((pair? expr) (concatenate (map expr->params (cdr expr))))
+    (else (list expr))))
+
+(define-syntax compile
+  (lambda (x)
+    (syntax-case x ()
+      ((k expr)
+       #`(begin
+           (define-method
+             (#,(datum->syntax
+                  #'k
+                  (expr->descr (syntax->datum #'expr)))) 0)
+           (#,(datum->syntax
+                #'k
+                (expr->descr (syntax->datum #'expr)))))))))
+
+; * define-method (descr (a <element>) (b <element>))
+;    (make (coerce (class-of a) (class-of b)) #:value (+ (get-value a) (get-value b))))
+
+; (define-syntax t (lambda (x) (syntax-case x () ((k expr) (datum->syntax #'k (expr->call (syntax->datum #'expr)))))))
+
+(define (ttt descr expr)
+  (list 'define-method (cons descr (syntax->datum (generate-temporaries expr)))
+        (make <int> #:value (+ (get-value a) (get-value b)))))
+
+(define (f) (compile (+ i j)))
+(format #t "~s~&" (f))
+(format #t "~s~&" (f))
+(format #t "~s~&" (f))
+;(format #t "~s~&" <+_?_?>)
+
+;(define-syntax compile (lambda (x) (syntax-case x () ((k expr) #`(syntax->datum #'expr)))))
+
