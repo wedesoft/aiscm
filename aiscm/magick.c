f4b8ecffec21aa90d665a50ac322b0ec623ec205
Jan Wedekind (Dr) <jan@wedesoft.de>
2016-01-19 17:46:20 +0000

    Switch to ImageMagick core

--- aiscm/magick.c
+++ aiscm/magick.c
@@ -1,5 +1,14 @@
 #include <libguile.h>
+#include <magick/MagickCore.h>
+
+SCM magick_read_image(SCM scm_fileName)
+{
+  SCM retval;
+  retval = scm_from_int(1234);
+  return retval;
+}
 
 void init_magick(void)
 {
+  scm_c_define_gsubr("magick-read-image", 1, 0, 0, magick_read_image);
 }
