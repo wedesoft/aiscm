72ee27da7bb0bcedfebdfcc49b24145228660c34
Jan Wedekind (Dr) <jan@wedesoft.de>
2016-01-21 21:25:54 +0000

    Use image list when writing images

--- etc/writeimg.c
+++ etc/writeimg.c
@@ -17,10 +17,11 @@ int main(void)
   GetImageInfo(image_info);
   Image *image = ConstituteImage(width, height, "I", CharPixel, buf, exception_info);
   CatchException(exception_info);
-  CopyMagickString(image->filename, file_name, MaxTextExtent);
-  WriteImage(image_info, image);
-  CatchException(&image->exception);
-  DestroyImage(image);
+  Image *images = NewImageList();
+  AppendImageToList(&images, image);
+  WriteImages(image_info, images, file_name, exception_info);
+  CatchException(exception_info);
+  DestroyImageList(images);
   free(buf);
   DestroyImageInfo(image_info);
   DestroyExceptionInfo(exception_info);
