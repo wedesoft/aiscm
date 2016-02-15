a6af34f94caf8d65d27016a537a88978c900c986
Jan Wedekind (Dr) <jan@wedesoft.de>
2016-02-15 21:42:14 +0000

    Playing around with tensor expressions

--- etc/env.scm
+++ etc/env.scm
@@ -4,9 +4,9 @@
   (term   #:init-keyword #:term   #:getter term)
   (index  #:init-keyword #:index  #:getter index)
   (stride #:init-keyword #:stride #:getter stride)
-  (lshape #:init-keyword #:lshape #:getter lshape))
-(define (lookup term index stride lshape)
-  (make <lookup> #:term term #:index index #:stride stride #:lshape lshape))
+  (last-shape #:init-keyword #:last-shape #:getter last-shape))
+(define (lookup term index stride last-shape)
+  (make <lookup> #:term term #:index index #:stride stride #:last-shape last-shape))
 
 (define-class <tensor> ()
   (index #:init-keyword #:index #:getter index)
@@ -26,18 +26,29 @@
 
 (define-method (element-wise (self <tensor>))
   (element-wise (term self) (index self)))
-;(define-method (ppp (self <tensor>)) (ppp (term self)))
 (define-method (typecode (self <lookup>)) (typecode (term self)))
 (define-method (ppp (self <lookup>)) (ppp (term self)))
-(define-method (ppp (self <pointer<>>)) self)
-(define-method (lshape (self <tensor>)) (lshape (term self)))
+(define-method (ppp (self <pointer<>>)) (get self))
+(define-method (type (self <pointer<>>)) (typecode self))
+(define-method (type (self <lookup>)) (type (term self)))
+(define-method (type (self <tensor>)) (sequence (type (term self))))
+(define-method (last-shape (self <tensor>)) (last-shape (term self)))
+(define-method (last-shape (self <sequence<>>)) (last (shape self)))
 (define-method (subst (self <pointer<>>) (p <var>))
   (make (class-of self) #:value p))
+(define-method (element-wise (s <sequence<>>))
+  (let [(incr (var <long>))
+        (p    (var <long>))]
+    (make <elementwise>
+          #:setup (list (IMUL incr (last (strides s)) (size-of (typecode s)))
+                        (MOV p (slot-ref s 'value)))
+          #:increment (list (ADD p incr))
+          #:body (project (rebase p s)))))
 (define-method (element-wise (self <lookup>))
   (let [(incr (var <long>))
         (p    (var <long>))]
     (make <elementwise>
-      #:setup (list (IMUL incr (stride self)) (size-of (typecode self))
+      #:setup (list (IMUL incr (stride self) (size-of (typecode self)))
                     (MOV p (ppp self)))
       #:increment (list (ADD p incr))
       #:body (subst (term self) p))))
@@ -50,188 +61,60 @@
 (define-method (store (p <pointer<>>) (a <pointer<>>))
   (let [(result (var (typecode a)))]
     (list (MOV result (ptr (typecode a) (get a))) (MOV (ptr (typecode p) (get p)) result))))
-(define-method (store (result <tensor>) expr)
+(define-method (store (result <sequence<>>) expr)
   (let [(destination (element-wise result))
         (source      (element-wise expr))]
     (list (get-setup destination)
           (get-setup source)
-          (repeat (lshape result)
+          (repeat (last-shape result)
                   (append (store (get-body destination) (get-body source)))
                           (get-increment destination)
                           (get-increment source)))))
+
+(define ctx (make <context>))
 
 (define r (skeleton (sequence <int>)))
 (define s (skeleton (sequence <int>)))
 (define c (skeleton <int>))
 (define i (var <int>))
 
+(define-method (returnable self) #f)
+(define-method (returnable (self <meta<int<>>>)) self)
+(define (assemble retval vars frag)
+  (virtual-variables (if (returnable (class-of retval)) (list (get retval)) '())
+                     (concatenate (map content (if (returnable (class-of retval)) vars (cons retval vars))))
+                     (append (store retval frag) (list (RET)))))
+(define (jit context classes proc)
+  (let* [(vars        (map skeleton classes))
+         (frag        (apply proc (map express vars)))
+         (result-type (type frag))
+         (return-type (returnable result-type))
+         (target      (if return-type result-type (pointer result-type)))
+         (retval      (skeleton target))
+         (args        (if return-type vars (cons retval vars)))
+         (code        (asm context
+                           (or return-type <null>)
+                           (map typecode (concatenate (map content args)))
+                           (assemble retval vars frag)))
+         (fun         (lambda header (apply code (concatenate (map content header)))))]
+    (if return-type
+        (lambda args
+          (let [(result (apply fun args))]
+            (get (build result-type result))))
+        (lambda args
+          (let [(result (make target #:shape (argmax length (map shape args))))]
+            (apply fun (cons result args))
+            (get (build result-type result)))))))
+
 (element-wise (express s))
 
-(store (express r) (express s))
+(store r (express s))
+
+(assemble r (list s) (express s))
+
+((jit ctx (list (sequence <int>)) (lambda (s) s)) (seq <int> 2 3 5))
 
 (ok (eq? c (express c))
     "Scalar expresses itself")
 (ok (is-a? (express s) <tensor>)
     "Sequence is expressed as tensor")
-
-
-(define-method (parameter self)
-  (make (fragment (class-of self)) #:args (list self) #:name parameter #:code '() #:value (get self)))
-(define-method (element-wise self)
-  (make <elementwise> #:setup '() #:increment '() #:body self))
-(define-method (element-wise (s <sequence<>>))
-  (let [(incr (var <long>))
-        (p    (var <long>))]
-    (make <elementwise>
-          #:setup (list (IMUL incr (last (strides s)) (size-of (typecode s)))
-                        (MOV p (slot-ref s 'value)))
-          #:increment (list (ADD p incr))
-          #:body (project (rebase p s)))))
-(define-method (element-wise (self <fragment<sequence<>>>))
-  (let [(loops (map element-wise (get-args self)))]
-    (make <elementwise>
-          #:setup (map get-setup loops)
-          #:increment (map get-increment loops)
-          #:body (apply (get-name self) (map get-body loops)))))
-(define-method (store (s <sequence<>>) (a <fragment<sequence<>>>))
-  (let [(destination (element-wise s))
-        (source      (element-wise a))]
-    (list (get-setup destination)
-          (get-setup source)
-          (repeat (last (shape s))
-                  (append (store (get-body destination) (get-body source))
-                          (get-increment destination)
-                          (get-increment source))))))
-
-
-(define-class <lookup> ()
-  (body #:init-keyword #:body #:getter get-body)
-  (var #:init-keyword #:var #:getter get-var))
-(define-method (get (self <fragment<sequence<>>>) (i <var>))
-  (make <lookup> #:body self #:var i))
-;(define-method (get (self <fragment<sequence<>>>) (i <var>))
-;  (make (fragment (project (type self)))
-;        #:args (list self i)
-;        #:name get
-;        #:code '()
-;        #:value self))
-
-(define (index i expr)
-  (get-body expr))
-
-(define ctx (make <context>))
-((jit ctx (list (sequence <int>)) identity) (seq <int> 2 3 5))
-
-(define r (skeleton (sequence <int>)))
-(define s (parameter (skeleton (sequence <int>))))
-(define c (parameter (skeleton <int>)))
-
-(define i (var <int>))
-(to-list ((jit ctx (list (sequence <int>)) (lambda (s) (index i (get s i)))) (seq <int> 2 3 5)))
-
-
-(use-modules (oop goops) (srfi srfi-1) (srfi srfi-26) (ice-9 optargs) (ice-9 curried-definitions) (aiscm util) (aiscm element) (aiscm pointer) (aiscm mem) (aiscm sequence) (aiscm asm) (aiscm jit) (aiscm op) (aiscm int) (aiscm float) (aiscm rgb))
-
-(use-modules (ice-9 optargs))
-(define* (test #:key (x 1) (y 2) #:allow-other-keys #:rest r)
-  (list x y r))
-
-(define-syntax test
-  (syntax-rules ()
-    ((test ((a b) args ...))
-     (cons b (test (args ...))))
-    ((test  ())
-     '())))
-
-(define-syntax tensor-list
-  (syntax-rules ()
-    ((_ arg args ...) (cons (tensor arg) (tensor-list args ...)))
-    ((_) '())))
-
-(define-syntax tensor
-  (syntax-rules (*)
-    ((_ (* args ...)) (cons '* (tensor-list args ...)))
-    ((_ arg)          arg)))
-
-(define s (seq 1 2 3 4))
-(define-syntax-rule (tensor sym args ...)
-  (if (is-a? sym <element>) (get sym args ...) (sym args ...)))
-(tensor + s 1)
-(tensor s 1)
-(tensor (seq 1 2 3 4) 1)
-(tensor + (s 1) 1)
-
-; arrays
-(make-array 0 2 3)
-(make-typed-array 'u8 0 2 3)
-#vu8(1 2 3)
-#2((1 2 3) (4 5 6))
-
-#2u32((1 2 3) (4 5 6))
-(define m #2s8((1 -2 3) (4 5 6)))
-(array-ref m 1 0)
-(array-shape m)
-(array-dimensions m)
-(array-rank m)
-(array->list m)
-
-; monkey patching
-(class-slots <x>)
-(define m (car (generic-function-methods test)))
-((method-procedure m) x)
-(slot-ref test 'methods)
-;(sort-applicable-methods test (compute-applicable-methods test (list x)) (list x))
-(equal? (map class-of (list x)) (method-specializers m))
-(define x (make <x>))
-(test x)
-(define-method (test (x <x>)) 'test2)
-(test x)
-
-(define (expr->descr expr)
-  (letrec
-    ((expr->str
-       (lambda (expr)
-         (cond
-           ((null? expr) "")
-           ((pair? expr) (format #f "<~a_~a>"
-                                 (car expr)
-                                 (string-join (map expr->str (cdr expr)) "_")))
-           (else "?")))))
-    (format #t "~s~&" expr)
-    (string->symbol (expr->str expr))))
-
-(define (expr->params expr)
-  (cond
-    ((null? expr) '())
-    ((pair? expr) (concatenate (map expr->params (cdr expr))))
-    (else (list expr))))
-
-(define-syntax compile
-  (lambda (x)
-    (syntax-case x ()
-      ((k expr)
-       #`(begin
-           (define-method
-             (#,(datum->syntax
-                  #'k
-                  (expr->descr (syntax->datum #'expr)))) 0)
-           (#,(datum->syntax
-                #'k
-                (expr->descr (syntax->datum #'expr)))))))))
-
-; * define-method (descr (a <element>) (b <element>))
-;    (make (coerce (class-of a) (class-of b)) #:value (+ (get-value a) (get-value b))))
-
-; (define-syntax t (lambda (x) (syntax-case x () ((k expr) (datum->syntax #'k (expr->call (syntax->datum #'expr)))))))
-
-(define (ttt descr expr)
-  (list 'define-method (cons descr (syntax->datum (generate-temporaries expr)))
-        (make <int> #:value (+ (get-value a) (get-value b)))))
-
-(define (f) (compile (+ i j)))
-(format #t "~s~&" (f))
-(format #t "~s~&" (f))
-(format #t "~s~&" (f))
-;(format #t "~s~&" <+_?_?>)
-
-;(define-syntax compile (lambda (x) (syntax-case x () ((k expr) #`(syntax->datum #'expr)))))
