f051f8f3dca118e9c29e9a3e4246809294a0715d
Jan Wedekind (Dr) <jan@wedesoft.de>
2016-10-22 21:52:16 +0100

    "deconstruct" method for splitting up composite values

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 ## Ready
 
-* rename "content" for jit -> unbuild; refactor "content" for composite values
 * (red (seq (rgb <obj>) (rgb 2 3 5)))
+* rename "content" for jit -> unbuild; refactor "content" for composite values
 * imag-part for object complex
 * (to-type <obj> (seq (rgb 1 2 3)))
 * (to-type (rgb <obj>) (seq (rgb 1 2 3)))
--- aiscm/composite.scm
+++ aiscm/composite.scm
@@ -1,10 +1,11 @@
 (define-module (aiscm composite)
-  #:use-module (oop goops) 
-  #:use-module (aiscm pointer) 
-  #:use-module (aiscm util) 
+  #:use-module (oop goops)
+  #:use-module (srfi srfi-26)
+  #:use-module (aiscm pointer)
+  #:use-module (aiscm util)
   #:use-module (aiscm element)
   #:export (<composite> <meta<composite>>
-            components component))
+            components deconstruct))
 
 (define-class* <composite> <element> <meta<composite>> <meta<element>>)
 (define-method (pointerless? (self <meta<composite>>)) (pointerless? (base self)))
@@ -12,3 +13,4 @@
 (define-method (component (type <meta<composite>>) self offset)
   (let* [(type (base (typecode self)))]
     (set-pointer-offset (pointer-cast type self) (* offset (size-of type)))))
+(define (deconstruct type self) (map (cut <> self) (components type)))
