3e2150f226b4690ff95e060726349ace2ee7bd40
Jan Wedekind (Dr) <jan@wedesoft.de>
2016-11-12 22:06:35 +0000

    Working on content of sequence parameter

--- etc/playground.scm
+++ etc/playground.scm
@@ -19,11 +19,6 @@
 
 (define lst (unbuild (sequence <int>) (sequence 2 3 5)))
 
-(define-method (build (type <meta<sequence<>>>) lst)
-  (make (sequence <int>) #:value (make <mem> #:base (make-pointer (last lst)) #:size 12)
-                         #:size (car lst)
-                         #:strides (list (cadr lst))))
-
 
 (define (content-s s) (list (parameter (make <long> #:value (dimension s)))
                             (parameter (make <long> #:value (stride s)))
@@ -36,5 +31,50 @@
 (diagnostics (f (seq <int> 2 3 5)))
 (diagnostics (build (sequence <int>) (f (seq <int> 2 3 5))))
 (diagnostics (build (sequence <int>) (unbuild (sequence <int>) (seq <int> 2 3 5))))
+
+(define (content-vars args) (map get (append-map content (map class-of args) args)))
+
+(define (assemble return-args args instructions)
+  "Determine result variables, argument variables, and instructions"
+  (list (content-vars return-args) (content-vars args) (attach instructions (RET))))
+
+(define (package-return-content value)
+  "Generate code to package parameter VALUE in a Scheme list"
+  (fold-right (cut native-call scm-cons <...>) (native-constant scm-eol) (content (type value) value)))
+
+(define (generate-return-code args expr)
+  (let [(result (parameter (type expr)))
+        (retval (skeleton <obj>))]
+    (list (list retval)
+          args
+          (insert-intermediate expr
+                               (parameter (type expr))
+                               (lambda (intermediate) (code (parameter retval) (package-return-content intermediate)))))))
+
+(define context (make <context>))
+(define classes (list (sequence <int>)))
+(define proc identity)
+(define vars        (map skeleton classes))
+(define expr        (apply proc (map parameter vars)))
+(define result-type (type expr))
+(define sequence?   (is-a? result-type <meta<sequence<>>>))
+(define result      (skeleton result-type))
+(define args        (if sequence? (cons result vars) vars))
+(define types       (map class-of args))
+
+(append-map (code (parameter result) expr) (package-return-content (parameter result)))
+
+(content (parameter (sequence <int>)))
+
+;(define code        (if sequence?
+;               (asm context
+;                    <null>
+;                    (map typecode (content-vars args))
+;                    (apply virtual-variables (assemble '() args (code (parameter result) expr))))
+;               (asm context
+;                    <ulong>
+;                    (map typecode (content-vars args))
+;                    (apply virtual-variables (apply assemble (generate-return-code args expr))))))
+;(fun         (lambda header (apply code (append-map unbuild types header))))
 
 (run-tests)
