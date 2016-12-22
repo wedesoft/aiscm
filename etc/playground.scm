342c1158f599cb62f07835dea711d031da38b68f
Jan Wedekind (Dr) <jan@wedesoft.de>
2016-12-22 22:38:01 +0000

    Initial stack parameter locations

--- etc/playground.scm
+++ etc/playground.scm
@@ -23,20 +23,21 @@
   "Get a list of parameters which need to be fetched"
   (filter (cut assq-ref allocation <>) (drop-up-to parameters 6)))
 
-(define (initial-parameter-locations parameters)
+(define (register-parameter-locations parameters)
   "Create an association list with the initial parameter locations"
-  (map (lambda (parameter register)
-         (cons parameter (to-type (typecode parameter) register)))
+  (map (lambda (parameter register) (cons parameter (to-type (typecode parameter) register)))
        parameters
        (list RDI RSI RDX RCX R8 R9)))
 
-(define (write-spilled-parameters parameters locations)
-  "Generate spill code for spilled parameters"
-  (map MOV (map (cut assq-ref locations <>) parameters) parameters))
+(define (stack-parameter-locations parameters offset)
+  "Determine initial locations of stack parameters"
+  (map (lambda (parameter index) (cons parameter (ptr (typecode parameter) RSP index)))
+       parameters
+       (iota (length parameters) (+ 8 offset) 8)))
 
-(define (fetch-stack-parameters parameters locations)
-  "Generate fetch code for stack parameters"
-  '())
+(define (update-parameter-locations parameters locations initial-locations)
+  "Generate spill code for spilled parameters"
+  (map MOV (map (cut assq-ref locations <>) parameters) (map (cut assq-ref initial-locations <>) parameters)))
 
 ; TODO: create and use array of initial parameter locations
 
@@ -54,19 +55,28 @@
       "stack parameter without allocated register does not need fetching")
   (ok (equal? '(a b c d e f) (parameters-to-spill '(a b c d e f g) '()))
       "only the first six parameters may need spilling")
-  (ok (equal? '() (initial-parameter-locations '()))
+  (ok (equal? '() (register-parameter-locations '()))
       "initial parameter locations for no parameters")
-  (ok (equal? (list (cons i EDI)) (initial-parameter-locations (list i)))
+  (ok (equal? (list (cons i EDI)) (register-parameter-locations (list i)))
       "initial parameter location for one integer parameter")
-  (ok (equal? (list (cons l RDI)) (initial-parameter-locations (list l)))
+  (ok (equal? (list (cons l RDI)) (register-parameter-locations (list l)))
       "initial parameter location for one long integer parameter")
-  (ok (equal? (list RDI RSI RDX RCX R8 R9) (map cdr (initial-parameter-locations (make-list 6 l))))
+  (ok (equal? (list RDI RSI RDX RCX R8 R9) (map cdr (register-parameter-locations (make-list 6 l))))
       "initial parameter locations for first six parameters")
-  (ok (equal? '() (write-spilled-parameters '() '()))
+  (ok (equal? '() (stack-parameter-locations '() 0))
+      "initial stack parameter locations for no parameters")
+  (ok (equal? (list (cons i (ptr <int> RSP 8))) (stack-parameter-locations (list i) 0))
+      "initial parameter location of an integer stack parameter")
+  (ok (equal? (list (cons l (ptr <long> RSP 8))) (stack-parameter-locations (list l) 0))
+      "initial parameter location of a long integer stack parameter")
+  (ok (equal? (list (ptr <int> RSP 8) (ptr <int> RSP 16)) (map cdr (stack-parameter-locations (list i i) 0)))
+      "parameter locations of two stack parameters")
+  (ok (equal? (list (ptr <int> RSP 24) (ptr <int> RSP 32)) (map cdr (stack-parameter-locations (list i i) 16)))
+      "take stack offset into account when determining stack parameter locations")
+  (ok (equal? '() (update-parameter-locations '() '() '()))
       "write no parameter to stack")
-  (ok (equal? (list (MOV (ptr <int> RSP -8) i)) (write-spilled-parameters (list i) (list (cons i (ptr <int> RSP -8)))))
-      "write one parameter to stack")
-  (ok (equal? '() (fetch-stack-parameters '() '()))
-      "no parameters to fetch"))
+  (ok (equal? (list (MOV (ptr <int> RSP -8) EDI))
+              (update-parameter-locations (list i) (list (cons i (ptr <int> RSP -8))) (list (cons i EDI))))
+      "write one parameter to stack"))
 
 (run-tests)
--- etc/t1.c
+++ etc/t1.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 
-int f(int a, int b, int c, int d, int e, int f, int g);
+int f(int a, int b, int c, int d, int e, int f, int g)
+{
+  return g;
+}
 
 int h(int a)
 {
