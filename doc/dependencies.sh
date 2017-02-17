4490f484a490951cd26adfc4b7765a56a7279553
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-02-17 17:28:59 +0000

    Trying libavresample-dev

--- doc/dependencies.sh
+++ doc/dependencies.sh
@@ -1,3 +1,3 @@
 sudo apt-get install -q --yes build-essential autoconf automake libtool
-sudo apt-get install -q --yes guile-2.0-dev linux-libc-dev gettext libmjpegtools-dev imagemagick libmagickcore-dev libpulse-dev libjpeg-dev libx11-dev libxext-dev libxv-dev libxmu-dev libxi-dev libxpm-dev libglu1-mesa-dev libgl1-mesa-dev libresample1-dev libswscale-dev libavformat-dev libavcodec-dev libavutil-dev pandoc
-# try 'libswresample-dev' if 'libresample1-dev' is not present
+sudo apt-get install -q --yes guile-2.0-dev linux-libc-dev gettext libmjpegtools-dev imagemagick libmagickcore-dev libpulse-dev libjpeg-dev libx11-dev libxext-dev libxv-dev libxmu-dev libxi-dev libxpm-dev libglu1-mesa-dev libgl1-mesa-dev libavresample-dev libswscale-dev libavformat-dev libavcodec-dev libavutil-dev pandoc
+# try 'libswresample-dev' if 'libavresample-dev' is not present
