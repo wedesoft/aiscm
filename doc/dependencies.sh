0b7b940f01c60d4e44b40d4e58cc451baf37f2d7
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-02-17 16:56:00 +0000

    Package name is "libswresample1-dev" under Ubuntu 14.04

--- doc/dependencies.sh
+++ doc/dependencies.sh
@@ -1,2 +1,3 @@
 sudo apt-get install -q --yes build-essential autoconf automake libtool
-sudo apt-get install -q --yes guile-2.0-dev linux-libc-dev gettext libmjpegtools-dev imagemagick libmagickcore-dev libpulse-dev libjpeg-dev libx11-dev libxext-dev libxv-dev libxmu-dev libxi-dev libxpm-dev libglu1-mesa-dev libgl1-mesa-dev libswresample-dev libswscale-dev libavformat-dev libavcodec-dev libavutil-dev pandoc
+sudo apt-get install -q --yes guile-2.0-dev linux-libc-dev gettext libmjpegtools-dev imagemagick libmagickcore-dev libpulse-dev libjpeg-dev libx11-dev libxext-dev libxv-dev libxmu-dev libxi-dev libxpm-dev libglu1-mesa-dev libgl1-mesa-dev libswresample1-dev libswscale-dev libavformat-dev libavcodec-dev libavutil-dev pandoc
+# try 'libswresample-dev' if 'libswresample1-dev' is not present
