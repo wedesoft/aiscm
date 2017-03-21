050fc8f93d90b0d468af98aee480fc12857d2e9c
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-03-21 12:52:04 +0000

    Removed undefined 'count' from samples module

--- aiscm/samples.scm
+++ aiscm/samples.scm
@@ -12,7 +12,7 @@
   #:export (<samples> <meta<samples>>
             AV_SAMPLE_FMT_U8 AV_SAMPLE_FMT_S16 AV_SAMPLE_FMT_S32 AV_SAMPLE_FMT_FLT AV_SAMPLE_FMT_DBL
             AV_SAMPLE_FMT_U8P AV_SAMPLE_FMT_S16P AV_SAMPLE_FMT_S32P AV_SAMPLE_FMT_FLTP AV_SAMPLE_FMT_DBLP
-            count planar? to-samples convert-samples convert-samples-from! type+planar->sample-format
+            planar? to-samples convert-samples convert-samples-from! type+planar->sample-format
             sample-format->type sample-format->planar)
   #:re-export (typecode shape channels rate to-array size-of))
 
