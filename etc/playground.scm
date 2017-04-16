2ac995fe2a5b443f8c13eaf4a7690513a040104d
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-04-16 21:35:58 +0100

    Reconstruct tensor expressions

--- etc/playground.scm
+++ etc/playground.scm
@@ -18,20 +18,7 @@
 
 (test-begin "playground")
 
-;(define ctx (make <context>))
-
-;(define-method (tensor-op name . args)
-;  (if (not (defined? name))
-;    (let [(f (jit ctx (map class-of args) (car args)))]
-;      (add-method! name (make <method> #:specializers (map class-of args)
-;                                       #:procedure (lambda args (apply f args))))))
-;  (apply (eval name (current-module)) args))
-;
-;(define-macro (tensor expr) `(tensor-op '- ,expr))
-;
-;(define s (seq 2 3 5))
-;
-;(tensor s)
+(define ctx (make <context>))
 
 (define (tensor-operation? expr)
   "Check whether expression is a tensor operation"
@@ -45,13 +32,40 @@
 
 (define (tensor-variables expr)
   "Return variables of tensor expression"
-  (if (tensor-operation? expr) (map tensor-variables (cdr expr)) expr))
+  (if (tensor-operation? expr) (append-map tensor-variables (cdr expr)) (list expr)))
+
+(define (consume-variables identifier variables)
+  "Build arguments of expresssion and return remaining variables"
+  (if (null? identifier)
+    (cons identifier variables)
+    (let* [(head (build-expression (car identifier) variables))
+           (tail (consume-variables (cdr identifier) (cdr head)))]
+      (cons (cons (car head) (car tail)) (cdr tail)))))
+
+(define (build-expression identifier variables)
+  "Build a tensor expression and return remaining variables"
+  (if (list? identifier)
+    (let [(arguments (consume-variables (cdr identifier) variables))]
+      (cons (cons (car identifier) (car arguments)) (cdr arguments)))
+    variables))
 
 (define (identifier->expression identifier variables)
   "Convert identifier to tensor expression with variables"
-  (if (list? identifier)
-      (cons (car identifier) (map identifier->expression (cdr identifier) variables))
-      variables))
+  (car (build-expression identifier variables)))
+
+;(define-macro (tensor-op expr)
+;  (let* [(vars (flatten (list (tensor-variables expr))))
+;         (args (symbol-list (length vars)))
+;         (identifier (expression->identifier expr))]
+;    `(let [(f (jit ctx (list . ,(map class-of vars)) (lambda ,args ,(identifier->expression identifier (car args)))))]
+;      (apply f ,(car vars)))))
+;
+;(define s (seq 2 3 5))
+;
+;(tensor-op s)
+;
+;(expression->identifier s)
+;(tensor-variables s)
 
 (test-begin "identify tensor operations")
   (test-assert "+ is a tensor operation"
@@ -83,9 +97,9 @@
 
 (test-begin "extract variables of tensor expression")
   (test-equal "detect variable name"
-    'x (tensor-variables 'x))
+    '(x) (tensor-variables 'x))
   (test-equal "detect numerical arguments"
-    42 (tensor-variables 42))
+    '(42) (tensor-variables 42))
   (test-equal "extract argument of unary plus"
     '(x) (tensor-variables '(+ x)))
   (test-equal "extract argument of unary minus"
@@ -93,26 +107,32 @@
   (test-equal "extract arguments of binary plus"
     '(x y) (tensor-variables '(+ x y)))
   (test-equal "extract variables recursively"
-    '((x) y) (tensor-variables '(+ (- x) y)))
+    '(x y) (tensor-variables '(+ (- x) y)))
   (test-equal "extract non-tensor operations"
-    '(read-image "test.bmp") (tensor-variables '(read-image "test.bmp")))
+    '((read-image "test.bmp")) (tensor-variables '(read-image "test.bmp")))
 (test-end "extract variables of tensor expression")
 
 (test-begin "convert tensor identifier to tensor expression")
   (test-equal "single variable expression"
-    'x (identifier->expression '_ 'x))
-  (test-equal "use provided variable names"
-    'y (identifier->expression '_ 'y))
+    (cons 'x '(y z)) (build-expression '_ '(x y z)))
   (test-equal "reconstruct unary plus operation"
-    '(+ x) (identifier->expression '(+ _) '(x)))
-  (test-equal "reconstruct unary minus operation"
-    '(- x) (identifier->expression '(- _) '(x)))
-  (test-equal "reconstruct binary plus operation"
-    '(+ x y) (identifier->expression '(+ _ _) '(x y)))
+    (cons '(+ x) '(y)) (build-expression '(+ _) '(x y)))
+  (test-equal "build arguments of unary expression"
+    (cons '(x) '(y z)) (consume-variables '(_) '(x y z)))
+  (test-equal "build arguments of binary expression"
+    (cons '(x y) '(z)) (consume-variables '(_ _) '(x y z)))
+  (test-equal "build binary expression"
+    (cons '(+ x y) '(z)) (build-expression '(+ _ _) '(x y z)))
+  (test-equal "consume variables recursively"
+    (cons '((- x)) '(y z)) (consume-variables '((- _)) '(x y z)))
+  (test-equal "reconstruct single variable expression"
+    'x (identifier->expression '_ '(x)))
+  (test-equal "reconstruct expression"
+    '(- x y) (identifier->expression '(- _ _) '(x y)))
   (test-equal "reconstruct nested expressions"
-    '(+ (- x) y) (identifier->expression '(+ (- _) _) '((x) y)))
+    '(+ (- x) y) (identifier->expression '(+ (- _) _) '(x y)))
   (test-equal "insert non-tensor operations"
-    '(read-image "test.bmp") (identifier->expression '_ '(read-image "test.bmp")))
+    '(read-image "test.bmp") (identifier->expression '_ '((read-image "test.bmp"))))
 (test-end "convert tensor identifier to tensor expression")
 
 (test-end "playground")
