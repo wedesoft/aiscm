4c7694fcd0b0b86535755e73d28fd5f918f63c0b
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-05-03 22:24:08 +0100

    WIP 1-axis tensor sum

--- etc/playground.scm
+++ etc/playground.scm
@@ -26,6 +26,18 @@
   (let [(index (var <long>))]
     (injecter name index delegate)))
 
+(define-method (project (self <injecter>))
+  (injecter (name self) (index self) (project (delegate self))))
+
+(define-method (project (self <injecter>) (idx <var>))
+  (injecter (name self) (index self) (project (delegate self) idx)))
+
+(define-method (tensor-loop (self <injecter>))
+  (let [(t (tensor-loop (delegate self)))]
+    (make <tensor-loop> #:loop-details (loop-details t)
+                        #:body (injecter (name self) (index self) (body t)))))
+; TODO: tensor-loop with index
+
 (define-method (code (a <param>) (b <injecter>))
   (let [(t (tensor-loop (delegate b) (index b)))]
     (append
@@ -34,9 +46,20 @@
         (lambda (intermediate)
           (append (append-map loop-increment (loop-details t))
                   (repeat 1 (value (dimension-hint (index b)))
-                          (code intermediate (+ intermediate (body t)) )
+                          (code intermediate (+ intermediate (body t))); TODO: remove unnecessary copying
                           (append-map loop-increment (loop-details t)))
                   (code a intermediate)))))))
+
+(define-method (code (a <indexer>) (b <injecter>))
+  (let [(dest   (tensor-loop a))
+        (source (tensor-loop b))]
+    (append (append-map loop-setup (loop-details dest))
+            (append-map loop-setup (loop-details source))
+            (repeat 0
+                    (value (dimension a))
+                    (code (body dest) (body source))
+                    (append-map loop-increment (loop-details dest))
+                    (append-map loop-increment (loop-details source))))))
 
 (test-begin "tensor reduce")
   (let [(s (parameter (sequence <ubyte>)))
@@ -48,19 +71,38 @@
     (test-eqv "Sum elements using tensor expression"
       10 (tensor (inject + k (get (seq 2 3 5) k))))
     (test-eqv "Sum different elements using tensor expression"
-      12 (tensor (inject + k (get (seq 2 3 7) k)))))
+      12 (tensor (inject + k (get (seq 2 3 7) k))))
+    (test-eq "Project a tensor sum"
+      <ubyte> (type (project (inject + i (get m i)))))
+    (test-eq "Project a tensor sum by index"
+      <ubyte> (type (project (dim j (inject + i (get m i j))))))
+    (test-eq "typecode of array of integer sums is integer"
+      <ubyte> (typecode (car (loop-details (tensor-loop (inject + i (get m i)))))))
+    (test-eq "preserve injection when looping over array of sums"
+      <injecter> (class-of (body (tensor-loop (inject + i (get m i))))))
+    (test-equal "Tensor sum along one axis"
+      '(4 6 8) (to-list (tensor (inject + k (get (arr (1 2 3) (3 4 5)) k))))))
 (test-end "tensor reduce")
 
-(define i (var <long>))
-(define s (parameter (sequence <ubyte>)))
-(define expr (inject + i (get s i)))
+(define m (parameter (multiarray <ubyte> 2)))
+(define expr (inject + k (get m k)))
 
-(define t (tensor-loop (delegate expr) (index expr)))
+(define ctx (make <context>))
 
-(loop-details t)
+(jit ctx (list (multiarray <ubyte> 2)) (lambda (m) (inject + i (get m i))))
 
-;(define ctx (make <context>))
+(define context ctx)
+(define classes (list (multiarray <ubyte> 2)))
+(define proc (lambda (m) (inject + i (get m i))))
 
-;((jit ctx (list (sequence <ubyte>)) (lambda (s) (inject + i (get s i)))) (seq 2 3 5))
+(define args         (map skeleton classes))
+(define expr         (apply proc (map parameter args)))
+(define result-type  (type expr))
+(define intermediate (parameter result-type))
+(define types        (map class-of args))
+
+(code intermediate expr)
+
+;(tensor (inject + j (dim i (get (arr (1 2 3) (4 5 6)) j i))))
 
 (test-end "playground")
