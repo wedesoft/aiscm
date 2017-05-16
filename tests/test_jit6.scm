7dd19fa4abd2e5d60728c515a07ef9652241f88a
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-05-16 20:20:35 +0100

    Remove test_jit6.scm

--- tests/test_jit6.scm
+++ tests/test_jit6.scm
@@ -80,4 +80,30 @@
 (test-assert "allocate memory in compiled method"
   ((jit ctx (list <ulong>) scm-gc-malloc) 128))
 
+(test-begin "list operations")
+  (test-assert "+ is an operation"
+    (memv '+ operations))
+  (test-assert "- is an operation"
+    (memv '- operations))
+  (test-assert "* is an operation"
+    (memv '* operations))
+(test-end "list operations")
+
+(test-begin "loop code")
+  (let* [(iterator   (var <long>))
+         (step       (var <long>))
+         (stride     (parameter <long>))
+         (base       (var <long>))
+         (loop-ubyte (make <loop-detail> #:typecode <ubyte> #:iterator iterator #:step step #:stride stride #:base base))
+         (loop-usint (make <loop-detail> #:typecode <usint> #:iterator iterator #:step step #:stride stride #:base base))]
+  (test-equal "setup of array loop should define increment and initialise pointer"
+    (list (IMUL step (value stride) 1) (MOV iterator base))
+    (loop-setup loop-ubyte))
+  (test-equal "setup of array loop adjust the step size according to the array type"
+    (list (IMUL step (value stride) 2) (MOV iterator base))
+    (loop-setup loop-usint))
+  (test-equal "a loop increment should increment the loop iterator"
+    (list (ADD iterator step))
+    (loop-increment loop-ubyte)))
+(test-end "loop code")
 (test-end "aiscm jit6")
