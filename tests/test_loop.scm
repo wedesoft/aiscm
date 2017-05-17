e494520615638eab655bf94c730f17246af59462
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-05-17 08:06:17 +0100

    Move multi-loop tests into test_loop.scm

--- tests/test_loop.scm
+++ tests/test_loop.scm
@@ -17,7 +17,9 @@
 (use-modules (srfi srfi-64)
              (oop goops)
              (aiscm loop)
+             (aiscm element)
              (aiscm int)
+             (aiscm sequence)
              (aiscm asm)
              (aiscm variable)
              (aiscm expression)
@@ -25,6 +27,63 @@
 
 
 (test-begin "aiscm loop")
+(test-begin "1D loop with multiple iterators")
+  (let* [(s (parameter (sequence <ubyte>)))
+         (t (multi-loop s))
+         (l (car (loop-details t)))]
+    (test-assert "tensor layer of sequence has an iterator"
+      (is-a? (iterator l) <var>))
+    (test-eq "loop iterator is a 64 bit integer"
+      <long> (typecode (iterator l)))
+    (test-assert "tensor layer of sequence has an iterator"
+      (is-a? (step l) <var>))
+    (test-eq "step size is a 64 bit integer"
+      <long> (typecode (step l)))
+    (test-eq "body of trivial tensor is a (scalar) parameter"
+      <param> (class-of (body t)))
+    (test-eq "body of trivial tensor is rebased on iterator"
+      (iterator l) (value (body t)))
+    (test-eq "typecode of unsigned byte tensor is unsigned byte"
+      <ubyte> (typecode l))
+    (test-eq "typecode of short integer tensor is short integer"
+      <sint> (typecode (car (loop-details (multi-loop (parameter (sequence <sint>)))))))
+    (test-equal "stride of tensor is stride of input array"
+      (stride (delegate s)) (stride l))
+    (test-equal "base of tensor is base pointer of input array"
+      (value s) (base l)))
+(test-end "1D loop with multiple iterators")
+
+(test-begin "2D loop with multiple iterators")
+  (let* [(m (parameter (multiarray <ubyte> 2)))
+         (i (var <long>))
+         (j (var <long>))
+         (t (dim j (dim i (make-function + coerce list (list (get (get m i) j))))))]
+    (test-assert "tensor loop preserves inner index"
+      (is-a? (body (multi-loop m)) <indexer>))
+    (test-eq "inner index is second index of 2D array"
+      (index (delegate m)) (index (body (multi-loop m))))
+    (test-eq "inner dimension is second dimension of 2D array"
+      (dimension (delegate m)) (dimension (body (multi-loop m))))
+    (test-assert "preserve loop details when skipping indices"
+      (is-a? (car (loop-details (multi-loop m))) <loop-detail>))
+    (test-assert "body of 2D tensor drops inner lookup"
+      (is-a? (delegate (body (multi-loop m))) <lookup>))
+    (test-skip 1); + operator not available here
+    (test-assert "tensor loop should preserve 2nd index of transposed array"
+      (is-a? (delegate (body (multi-loop t))) <lookup>)))
+(test-end "2D loop with multiple iterators")
+
+(test-begin "zero-dimensional loop")
+  (let [(v (parameter <int>))
+        (i (var <long>))]
+    (test-assert "loop code for scalar tensor is empty"
+      (null? (loop-details (multi-loop v))))
+    (test-eq "body of scalar tensor is itself"
+      v (body (multi-loop v)))
+    (test-eq "scalar tensor ignores indices"
+      v (body (multi-loop v i))))
+(test-end "zero-dimensional loop")
+
 (test-begin "loop code")
   (let* [(iterator   (var <long>))
          (step       (var <long>))
--- tests/test_tensor.scm
+++ tests/test_tensor.scm
@@ -16,62 +16,6 @@
 
 (define ctx (make <context>))
 
-(test-begin "1D tensor")
-  (let* [(s (parameter (sequence <ubyte>)))
-         (t (multi-loop s))
-         (l (car (loop-details t)))]
-    (test-assert "tensor layer of sequence has an iterator"
-      (is-a? (iterator l) <var>))
-    (test-eq "loop iterator is a 64 bit integer"
-      <long> (typecode (iterator l)))
-    (test-assert "tensor layer of sequence has an iterator"
-      (is-a? (step l) <var>))
-    (test-eq "step size is a 64 bit integer"
-      <long> (typecode (step l)))
-    (test-eq "body of trivial tensor is a (scalar) parameter"
-      <param> (class-of (body t)))
-    (test-eq "body of trivial tensor is rebased on iterator"
-      (iterator l) (value (body t)))
-    (test-eq "typecode of unsigned byte tensor is unsigned byte"
-      <ubyte> (typecode l))
-    (test-eq "typecode of short integer tensor is short integer"
-      <sint> (typecode (car (loop-details (multi-loop (parameter (sequence <sint>)))))))
-    (test-equal "stride of tensor is stride of input array"
-      (stride (delegate s)) (stride l))
-    (test-equal "base of tensor is base pointer of input array"
-      (value s) (base l)))
-(test-end "1D tensor")
-
-(test-begin "2D tensor")
-  (let* [(m (parameter (multiarray <ubyte> 2)))
-         (i (var <long>))
-         (j (var <long>))
-         (t (dim j (dim i (+ (get (get m i) j)))))]
-    (test-assert "tensor loop preserves inner index"
-      (is-a? (body (multi-loop m)) <indexer>))
-    (test-eq "inner index is second index of 2D array"
-      (index (delegate m)) (index (body (multi-loop m))))
-    (test-eq "inner dimension is second dimension of 2D array"
-      (dimension (delegate m)) (dimension (body (multi-loop m))))
-    (test-assert "preserve loop details when skipping indices"
-      (is-a? (car (loop-details (multi-loop m))) <loop-detail>))
-    (test-assert "body of 2D tensor drops inner lookup"
-      (is-a? (delegate (body (multi-loop m))) <lookup>))
-    (test-assert "tensor loop should preserve 2nd index of transposed array"
-      (is-a? (delegate (body (multi-loop t))) <lookup>)))
-(test-end "2D tensor")
-
-(test-begin "scalar tensor")
-  (let [(v (parameter <int>))
-        (i (var <long>))]
-    (test-assert "loop code for scalar tensor is empty"
-      (null? (loop-details (multi-loop v))))
-    (test-eq "body of scalar tensor is itself"
-      v (body (multi-loop v)))
-    (test-eq "scalar tensor ignores indices"
-      v (body (multi-loop v i))))
-(test-end "scalar tensor")
-
 (test-begin "compiling tensors")
   (let [(s (seq <int> 2 3 5))
         (t (seq <int> 3 5 7))
