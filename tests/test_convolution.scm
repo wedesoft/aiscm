0209bbaa059b7e60bc2c134f18a879b741ebdec8
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-06-21 17:36:16 +0100

    Copied updated convolution code into library

--- tests/test_convolution.scm
+++ tests/test_convolution.scm
@@ -25,14 +25,20 @@
 (test-begin "aiscm convolution")
 (test-begin "convolution")
   (test-equal "trivial convolution"
-    '(2 3 5) (to-list (convolve (seq <int> 2 3 5) 1)))
+    '(2 3 5) (to-list (convolve (seq 2 3 5) 1)))
   (test-equal "use convolution to scale values"
-    '(4 6 10) (to-list (convolve (seq <int> 2 3 5) 2)))
+    '(4 6 10) (to-list (convolve (seq 2 3 5) 2)))
   (test-equal "convolution with one-element array"
-    '(4 6 10) (to-list (convolve (seq <int> 2 3 5) (seq <int> 2))))
+    '(4 6 10) (to-list (convolve (seq 2 3 5) (seq 2))))
+  (test-equal "do not read over array boundaries"
+    '(0 0 0) (to-list (convolve (crop 3 (dump 1 (seq 1 0 0 0 1))) (seq 1 2 4))))
   (test-equal "convolution with 3-element impulse kernel"
+    '(1 2 3 4 5) (to-list (convolve (seq 1 2 3 4 5) (seq 0 1 0))))
+  (test-equal "convolution with 32-bit integers"
     '(1 2 3 4 5) (to-list (convolve (seq <int> 1 2 3 4 5) (seq <int> 0 1 0))))
-  (test-equal "convolution with 3-element shift kernel"
-    '(2 3 0) (to-list (convolve (seq <int> 1 2 3) (seq <int> 1 0 0))))
+  (test-equal "convolution with 3-element shift-left kernel"
+    '(2 3 0) (to-list (convolve (seq 1 2 3) (seq 1 0 0))))
+  (test-equal "convolution with 3-element shift-right kernel"
+    '(0 1 2) (to-list (convolve (seq 1 2 3) (seq 0 0 1))))
 (test-end "convolution")
 (test-end "aiscm convolution")
