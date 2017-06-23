6349ca77e28950aec108a998e545ef183479a43a
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-06-23 17:27:50 +0100

    Documentation about convolution with complex numbers

--- doc/convolution.md
+++ doc/convolution.md
@@ -1,7 +1,8 @@
 # Array convolutions
 ## One-dimensional convolutions
 
-One dimensional signals such as audio samples can be filtered using a 1D convolution:
+One dimensional signals such as audio samples can be filtered using a 1D convolution.
+Convolution can also be performed using composite values such as complex numbers.
 
 ```Scheme
 @../tests/integration/convolution_1d.scm@
--- tests/integration/convolution_1d.scm
+++ tests/integration/convolution_1d.scm
@@ -1,4 +1,7 @@
-(use-modules (aiscm sequence) (aiscm convolution))
+(use-modules (aiscm sequence) (aiscm convolution) (aiscm complex))
 (convolve (seq 0 0 2 2 2 2 0 0) (seq 1 0 -1))
 ;#<sequence<int<16,signed>>>:
 ;(0 2 2 0 0 -2 -2 0)
+(convolve (seq 0 0 1 0 0 0 0 2 0 0) (seq +1 +i -1 -i))
+;#<sequence<complex<int<16,signed>>>>:
+;(1 0.0+1.0i -1 0.0-1.0i 0 2 0.0+2.0i -2 0.0-2.0i 0)
