405fef995f2cf3410f189d1c183a7921082309f0
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-07-06 22:41:47 +0100

    Prototyping register spill code

--- etc/playground.scm
+++ etc/playground.scm
@@ -1,7 +1,6 @@
 (use-modules (srfi srfi-64))
 (use-modules (oop goops) (aiscm element) (aiscm int) (aiscm asm) (aiscm command) (aiscm variable) (aiscm program) (aiscm compile) (aiscm util) (srfi srfi-1))
 
-
 (define (replace-variables allocation cmd temporaries)
   "Substitute variables with registers and add spill code using temporary registers if necessary"
   (let* [(primary-var (first-argument cmd))
@@ -26,15 +25,25 @@
   "Substitute variables with registers and add spill code using temporary registers if necessary"
   (let [(substituted (substitute-variables cmd allocation))]
     (if (is-a? substituted <cmd>)
-      (let* [(target    (car (append (output cmd) (input cmd))))
-             (location  (assq-ref allocation target))
-             (is-input  (memv target (input cmd)))
-             (is-output (memv target (output cmd)))]
-        (filter identity
-          (list (and is-input  (MOV (to-type (typecode target) (car temporaries)) location))
-                (substitute-variables cmd (list (cons target (car temporaries))))
-                (and is-output (MOV location (to-type (typecode target) (car temporaries)))))))
+      (let [(pointers (get-ptr-args cmd))]
+        (if (null? pointers)
+          (let* [(target    (car (append (output cmd) (input cmd)))); TODO: select spilled values
+                 (location  (assq-ref allocation target))
+                 (is-input  (memv target (input cmd)))
+                 (is-output (memv target (output cmd)))
+                 (temporary (car temporaries))
+                 (typed-tmp (to-type (typecode target) temporary))]
+            (filter identity
+              (list (and is-input  (MOV typed-tmp location)); TODO: select spilled values
+                    (substitute-variables cmd (list (cons target temporary))); TODO: replace other vars
+                    (and is-output (MOV location typed-tmp)))))
+          (let* [(target   (car pointers))
+                 (location (assq-ref allocation target))
+                 (temporary (car temporaries))]
+            (cons (MOV temporary location)
+                  (replace-variables allocation (substitute-variables cmd (list (cons target temporary))) (cdr temporaries))))))
       (list (substitute-variables cmd allocation)))))
+
 
 (test-begin "playground")
 (test-begin "replace-variables")
@@ -57,18 +66,21 @@
     (list (MOV EAX (ptr <int> RSP 16)) (PUSH EAX)) (replace-variables (list (cons a (ptr <long> RSP 16))) (PUSH a) (list RAX)))
   (test-equal "use intermediate register for spilled output value"
     (list (POP RAX) (MOV (ptr <long> RSP 16) RAX)) (replace-variables (list (cons l (ptr <long> RSP 16))) (POP l) (list RAX)))
+  ;(test-equal "read and write back argument from stack into temporary register"
+  ;  (list (MOV EAX (ptr <int> RSP 16)) (ADD EAX 1) (MOV (ptr <int> RSP 16) EAX))
+  ;  (replace-variables (list (cons a (ptr <long> RSP 16))) (ADD a 1) (list RAX)))
+  ;(test-equal "write output value in temporary register to the stack"
+  ;  (list (MOV EAX 1) (MOV (ptr <int> RSP 16) EAX)) (replace-variables (list (cons a (ptr <long> RSP 16))) (MOV a 1) (list RAX)))
+  (test-equal "fetch pointer value from stack"
+    (list (MOV RAX (ptr <long> RSP 32)) (MOV EDX (ptr <int> RAX 8)))
+    (replace-variables (list (cons a RDX) (cons p (ptr <long> RSP 32))) (MOV a (ptr <int> p 8)) (list RAX)))
 
-  (test-skip 6)
-  (test-equal "read and write back argument from stack into temporary register"
-    (list (MOV EAX (ptr <int> RSP 16)) (ADD EAX 1) (MOV (ptr <int> RSP 16) EAX))
-    (replace-variables (list (cons a (ptr <long> RSP 16))) (ADD a 1) (list RAX)))
-  (test-equal "write output value in temporary register to the stack"
-    (list (MOV EAX 1) (MOV (ptr <int> RSP 16) EAX)) (replace-variables (list (cons a (ptr <long> RSP 16))) (MOV a 1) (list RAX)))
+  (test-skip 3)
   (test-equal "use temporary variable to implement reading from pointer to pointer"
     (list (MOV RAX (ptr <long> RSP 32)) (MOV EDX (ptr <int> RAX 8)))
     (replace-variables (list (cons a RDX) (cons p (ptr <long> RSP 32))) (MOV a (ptr <int> p 8)) (list RAX)))
-  (test-equal "do not use temporary variable when reading from register pointer"
-    (list (MOV EDX (ptr <int> RCX 8))) (replace-variables (list (cons a RDX) (cons p RCX)) (MOV a (ptr <int> p 8)) (list RAX)))
+  ;(test-equal "do not use temporary variable when reading from register pointer"
+  ;  (list (MOV EDX (ptr <int> RCX 8))) (replace-variables (list (cons a RDX) (cons p RCX)) (MOV a (ptr <int> p 8)) (list RAX)))
   (test-equal "use temporary variable to implement writing to pointer to pointer"
     (list (MOV RAX (ptr <long> RSP 32)) (MOV (ptr <int> RAX 8) EDX))
     (replace-variables (list (cons a RDX) (cons p (ptr <long> RSP 32))) (MOV (ptr <int> p 8) a) (list RAX)))
