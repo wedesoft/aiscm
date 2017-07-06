c80a70827649db9948546a3a9b4ab98f1682ad38
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-07-06 12:51:23 +0100

    Prototyping replacing of spilled variables

--- etc/playground.scm
+++ etc/playground.scm
@@ -1,6 +1,7 @@
 (use-modules (srfi srfi-64))
 (use-modules (oop goops) (aiscm element) (aiscm int) (aiscm asm) (aiscm command) (aiscm variable) (aiscm program) (aiscm compile) (aiscm util) (srfi srfi-1))
 
+
 (define (replace-variables allocation cmd temporaries)
   "Substitute variables with registers and add spill code using temporary registers if necessary"
   (let* [(primary-var (first-argument cmd))
@@ -21,23 +22,43 @@
                 (substitute-variables cmd (fold (lambda (var tmp alist) (assq-set alist var tmp))
                                                 allocation spilled-pointers temporaries)))))))
 
+(define (replace-variables allocation cmd temporaries)
+  "Substitute variables with registers and add spill code using temporary registers if necessary"
+  (let [(substituted (substitute-variables cmd allocation))]
+    (if (is-a? substituted <cmd>)
+      (let* [(target    (car (append (output cmd) (input cmd))))
+             (location  (assq-ref allocation target))
+             (is-input  (memv target (input cmd)))
+             (is-output (memv target (output cmd)))]
+        (filter identity
+          (list (and is-input  (MOV (to-type (typecode target) (car temporaries)) location))
+                (substitute-variables cmd (list (cons target (car temporaries))))
+                (and is-output (MOV location (to-type (typecode target) (car temporaries)))))))
+      (list (substitute-variables cmd allocation)))))
+
 (test-begin "playground")
 (test-begin "replace-variables")
   (let [(a (var <int>))
         (x (var <sint>))
+        (l (var <long>))
         (p (var <long>))]
   (test-equal "put instruction into a list if there are no variables to replace"
     (list (MOV EAX 0)) (replace-variables '() (MOV EAX 0) '()))
   (test-equal "replace input variable with allocated register"
     (list (MOV ESI ECX)) (replace-variables (list (cons a RCX)) (MOV ESI a) '()))
-  (test-equal "replace output variable with allocated register"
-    (list (MOV ECX 0)) (replace-variables (list (cons a RCX)) (MOV a 0) '()))
-  (test-equal "read input variable from spill location"
-    (list (MOV EDX (ptr <int> RSP 16))) (replace-variables (list (cons a (ptr <long> RSP 16))) (MOV EDX a) '()))
-  (test-equal "use temporary register for first argument and fetch value from spill location"
-    (list (MOV AX (ptr <sint> RSP 16)) (CMP AX 0)) (replace-variables (list (cons x (ptr <long> RSP 16))) (CMP x 0) (list RAX)))
-  (test-equal "use correct type for temporary register"
-    (list (MOV EAX (ptr <int> RSP 16)) (CMP EAX 0)) (replace-variables (list (cons a (ptr <long> RSP 16))) (CMP a 0) (list RAX)))
+  ;(test-equal "replace output variable with allocated register"
+  ;  (list (MOV ECX 0)) (replace-variables (list (cons a RCX)) (MOV a 0) '()))
+  ;(test-equal "read input variable from spill location"
+  ;  (list (MOV EDX (ptr <int> RSP 16))) (replace-variables (list (cons a (ptr <long> RSP 16))) (MOV EDX a) '()))
+; PUSH
+  (test-equal "use intermediate register for spilled input value"
+    (list (MOV RAX (ptr <long> RSP 16)) (PUSH RAX)) (replace-variables (list (cons l (ptr <long> RSP 16))) (PUSH l) (list RAX)))
+  (test-equal "use correct type for intermediate register"
+    (list (MOV EAX (ptr <int> RSP 16)) (PUSH EAX)) (replace-variables (list (cons a (ptr <long> RSP 16))) (PUSH a) (list RAX)))
+  (test-equal "use intermediate register for spilled output value"
+    (list (POP RAX) (MOV (ptr <long> RSP 16) RAX)) (replace-variables (list (cons l (ptr <long> RSP 16))) (POP l) (list RAX)))
+
+  (test-skip 6)
   (test-equal "read and write back argument from stack into temporary register"
     (list (MOV EAX (ptr <int> RSP 16)) (ADD EAX 1) (MOV (ptr <int> RSP 16) EAX))
     (replace-variables (list (cons a (ptr <long> RSP 16))) (ADD a 1) (list RAX)))
