f2ccacbf4a74cd28f4573886ddbf36d83182d824
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-08-27 20:38:42 +0100

    Prototype code for compiled array assignment

--- etc/playground.scm
+++ etc/playground.scm
@@ -5,8 +5,11 @@
 
 (define ctx (make <context>))
 
-(define (set3 self value)
-  (let* [(classes      (list (class-of self) (class-of value)))
+(define-method (set3 (self <element>) value)
+  (slot-set! self 'value value))
+
+(define-method (set3 (self <sequence<>>) value)
+  (let* [(classes      (list (class-of self) (class-of (wrap value))))
          (args         (map skeleton classes))
          (parameters   (map parameter args))
          (commands     (virtual-variables '() (content-vars args) (attach (apply duplicate parameters) (RET))))
@@ -27,21 +30,17 @@
 (apply instructions (append-map unbuild classes header))
 s
 
-(define-method (set2 (self <element>) value) (begin (slot-set! self 'value value)) value)
-(define-method (set2 (self <sequence<>>) value) (store self value) value)
-(define-method (set2 (self <sequence<>>) (value <sequence<>>)) 0)
-
 (test-begin "playground")
 (test-eqv "set value of integer"
-  123 (let [(i (make <int> #:value 0))] (set2 i 123) (get i)))
+  123 (let [(i (make <int> #:value 0))] (set3 i 123) (get i)))
 (test-eqv "return value after setting integer"
-  123 (set2 (make <int> #:value 0) 123))
+  123 (set3 (make <int> #:value 0) 123))
 (let [(s (seq 1 2 3))]
-  (set2 s 0)
+  (set3 s 0)
   (test-equal "fill array with zeros"
     '(0 0 0) (to-list s))
   (test-eqv "return value after setting array"
-    0 (set2 s 0))
+    0 (set3 s 0))
   (set3 s (seq 2 3 5))
   (test-equal "copy content of other array"
     '(2 3 5) (to-list s)))
