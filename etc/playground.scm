8fb3860ce51ce017b80ad6a9dde43f71b87edc0c
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-08-29 17:34:53 +0100

    Get range of values from 1D array

--- etc/playground.scm
+++ etc/playground.scm
@@ -2,9 +2,13 @@
 (use-modules (srfi srfi-1))
 (use-modules (oop goops) (aiscm asm) (aiscm jit) (aiscm element) (aiscm int) (aiscm sequence) (aiscm pointer) (aiscm expression) (aiscm operation) (aiscm util))
 
-(define (element offset self) (project (dump offset self)))
+(define-method (element (offset <integer>) self) (project (dump offset self)))
+(define-method (element (offset <pair>) self) (crop (- (cdr offset) (car offset)) (dump (car offset) self)))
 
 (define ctx (make <context>))
+
+; TODO: cache
+; TODO: ranges for element, set, and get
 
 (define (compiled-copy self value)
   (let* [(classes      (list (class-of self) (class-of (wrap value))))
@@ -14,6 +18,9 @@
            (instructions (asm ctx <null> (map typecode (content-vars args)) commands))]
       (apply instructions (append-map unbuild classes (list self value)))
       value))
+
+(define-method (get (self <sequence<>>) . args)
+  (if (null? args) self (get (fetch (fold-right element self args)))))
 
 (define-method (set3 (self <element>) value)
   (slot-set! self 'value value))
@@ -25,6 +32,19 @@
   (compiled-copy (fold-right element self (all-but-last args)) (last args)))
 
 (test-begin "playground")
+(test-equal "'get' without additional arguments should return the sequence itself"
+  '(1 2 3) (to-list (get (seq 1 2 3))))
+(test-equal "Content of converted 2D array"
+  '((1 2 3) (4 5 6)) (to-list (arr (1 2 3) (4 5 6))))
+(test-equal "Getting row of 2D array"
+  '(4 5 6) (to-list (get (arr (1 2 3) (4 5 6)) 1)))
+(test-equal "Getting element of 2D array with one call to 'get'"
+  2 (get (arr (1 2 3) (4 5 6)) 1 0))
+(test-equal "Getting element of 2D array with two calls to 'get'"
+  2 (get (get (arr (1 2 3) (4 5 6)) 0) 1))
+(test-equal "Get range of values from 1D array"
+  '(3 5) (to-list (get (seq 2 3 5 7) '(1 . 3))))
+
 (test-eqv "set value of integer"
   123 (let [(i (make <int> #:value 0))] (set3 i 123) (get i)))
 (test-eqv "return value after setting integer"
