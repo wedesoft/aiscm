659640ce42ee54af3c39c88df4a0770b0484f4ba
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-14 13:12:47 +0000

    Use state variable

--- etc/rigs.scm
+++ etc/rigs.scm
@@ -1,102 +1,114 @@
 #!/usr/bin/env guile
 !#
-(use-modules (glut) (gl) (gl low-level) (glu) (srfi srfi-1))
+; https://www.youtube.com/watch?v=GnzzmA9x1q4
+(use-modules (glut) (gl) (gl low-level) (glu) (srfi srfi-1) (srfi srfi-26))
 
-(define pi 3.1415926)
+(define pi 3.141592653589793)
+(define g 20)
+(define drag -0.5)
+(define damp 2)
+(define stiff 200)
 (define (sqr x) (* x x))
 (define (int v) (inexact->exact (round v)))
+(define (vector a b) (map - b a))
+(define (distance a b) (sqrt (apply + (map sqr (vector a b)))))
 
+(define dt 0.01)
 (define n   24)
 (define v  200)
 (define r1  30)
 (define r2  70)
+(define cx 100)
+(define cy 100)
 
-(define f (/ r1 r2))
+(define n2 (/ n 2))
 
-(define vertex (apply append (map (lambda (i)
-  (let* [(a (/ (* 2 pi i) (/ n 2)))
-         (ca (cos a))
-         (sa (sin a))]
-    (list (list (+ 100 (* ca r1)) (+ 100 (* sa r1)) (* -1 f sa v) (* f ca v))
-          (list (+ 100 (* ca r2)) (+ 100 (* sa r2)) (* -1 sa v) (* ca v)))))
-      (iota (/ n 2)))))
+; speed values
+; accelerations for given positions and speeds
+; 4th order Runger-Kutta 4
 
-(define (clip x) (if (< x 0) (+ x n) (if (>= x n) (- x n) x)))
+(define angles (map (cut * 2 (/ pi n2) <>) (iota n)))
+(define radius (append (make-list n2 r1) (make-list n2 r2)))
 
-(define connect (apply append (map (lambda (i)
-  (list (map (lambda (j) (clip (+ (* 2 i) j))) '(-2 -1 1 3 2))
-        (map (lambda (j) (clip (+ (* 2 i) j))) '(-2 -1 0 2 3))))
-  (iota (/ n 2)))))
+(define vertex (map (lambda (a r) (list (+ cx (* r (cos a))) (+ cy (* r (sin a)))) ) angles radius))
+(define speed (make-list n '(0 0)))
 
-(define distance (map (lambda (i l)
-  (map (lambda (j)
-    (let [(a (list-ref vertex i))
-          (b (list-ref vertex j))]
-      (sqrt (+ (sqr (- (car b) (car a))) (sqr (- (cadr b) (cadr a))))))) l)) (iota n) connect))
+(define (inner-index index offset) (remainder (+ index offset n2) n2))
+(define (outer-index index offset) (+ (remainder (+ index offset n2) n2) n2))
+(define (neighbours index)
+  (delete index
+          (append (map (cut inner-index index <>) '(-1 0 1))
+                  (map (cut outer-index index <>) '(-1 0 1)))))
+(define edge (map neighbours (iota n)))
 
-(define l 30)
-(define dt 0.01)
-(define g 20)
-(define drag -0.1)
-(define damp 5)
-(define stiff 150)
+(define (adjacent vertex) (map (cut map (cut list-ref vertex <>) <>) edge))
+(define nominal (map (lambda (u vs) (map (cut distance u <>) vs)) vertex (adjacent vertex)))
 
-(define main-window #t)
+(define (scale node factor) (map (cut * factor <>) node))
+(define (scale-all nodes dt) (map (cut scale <> dt) nodes))
+
+(define (add node delta) (map + node delta))
+(define (add-all nodes delta) (map add nodes delta))
+
+(define (relative-speed u du v dv)
+  (/ (apply + (map * (vector u v) (vector du dv))) (distance u v)))
+
+(define (spring u du v dv l)
+  (let* [(distance   (distance u v))
+         (displace   (- distance l))
+         (speed      (relative-speed u du v dv))
+         (accelerate (+ (* stiff displace) (* damp speed)))]
+    (scale (vector u v) (/ accelerate distance))))
+
+(define (acceleration vertex speed vs dvs ls)
+  (add (reduce add '(0 0) (map (cut spring vertex speed <...>) vs dvs ls))
+       (add (scale speed drag) (list 0 g))))
+
+(define (acceleration-all vertex)
+  (map acceleration vertex speed (adjacent vertex) (adjacent speed) nominal))
+
+(define (state-all vertex speed) (map append vertex speed))
+(define (vertex-state state) (map (cut take <> 2) state))
+(define (speed-state state) (map (cut drop <> 2) state))
+
+(define (change-all state)
+  (map append (speed-state state) (acceleration-all (vertex-state state))))
+
+(define main-window #f)
 
 (define (on-reshape width height)
   (pk 'reshape! width height)
   (gl-viewport 0 0 width height)
   (set-gl-matrix-mode (matrix-mode projection))
   (gl-load-identity)
-  (gl-ortho 0 width height 0 -1 +1)
-)
+  (gl-ortho 0 width height 0 -1 +1))
 
-(define (change a b l)
-  (let* [(x1  (car    a))
-         (y1  (cadr   a))
-         (x2  (car    b))
-         (y2  (cadr   b))
-         (vx1 (caddr  a))
-         (vy1 (cadddr a))
-         (vx2 (caddr  b))
-         (vy2 (cadddr b))
-         (d  (sqrt (+ (sqr (- x2 x1)) (sqr (- y2 y1)))))
-         (rv (/ (+ (* (- vx2 vx1) (- x2 x1)) (* (- vy2 vy1) (- y2 y1))) d))
-         (s (- d l))
-         (h (+ (* stiff s) (* damp rv)))
-         (ax (* h (/ (- x2 x1) d)))
-         (ay (* h (/ (- y2 y1) d)))]
-    (list ax ay)))
+(define (on-idle)
+  (let* [(state  (state-all vertex speed))
+         (statex (add-all state (scale-all (change-all state) dt)))]
+    (set! speed (speed-state statex))
+    (set! vertex (vertex-state statex)))
+  (set! vertex (map (lambda (v s) (if (and (>= (cadr v) 470) (> (cadr s) 0)) (list (car v) 470) v)) vertex speed))
+  (set! speed (map (lambda (v s) (if (and (>= (cadr v) 470) (> (cadr s) 0)) (list 0 0) s)) vertex speed))
+  (post-redisplay))
+
+(define (gl-vertex-2d v) (gl-vertex (car v) (cadr v) 0))
 
 (define (on-display)
   (gl-clear (clear-buffer-mask color-buffer))
-  (glPointSize 10)
+  (glPointSize 5)
   (gl-begin (begin-mode points)
     (gl-color 1 0 0)
-    (for-each (lambda (v) (gl-vertex (car v) (cadr v) 0)) vertex))
+    (for-each gl-vertex-2d vertex))
   (gl-begin (begin-mode lines)
     (gl-color 0 1 0)
-    (for-each (lambda (i)
-      (for-each (lambda (j)
-        (gl-vertex (car (list-ref vertex i)) (cadr (list-ref vertex i)) 0)
-        (gl-vertex (car (list-ref vertex j)) (cadr (list-ref vertex j)) 0))
-      (list-ref connect i)))
-      (iota n)))
+    (for-each (lambda (u vs) (for-each (lambda (v) (gl-vertex-2d u) (gl-vertex-2d v)) vs)) vertex (adjacent vertex)))
   (swap-buffers))
-
-(define (on-idle)
-   (let [( acc (map (lambda (i) (reduce (lambda (a b) (map + a b)) '(0 0) (map (lambda (j l) (change (list-ref vertex i) (list-ref vertex j) l)) (list-ref connect i) (list-ref distance i)))) (iota n)))]
-     (set! vertex (map (lambda (v a) (list (+ (car v) (* (caddr v) dt))
-                                           (+ (cadr v) (* (cadddr v) dt))
-                                           (+ (caddr v) (* (+ (car a) (* drag (caddr v))) dt))
-                                           (+ (cadddr v) (* (+ g (cadr a) (* drag (cadddr v))) dt)))) vertex acc))
-     (set! vertex (map (lambda (v) (if (and (>= (cadr v) 470) (> (cadddr v) 0)) (list (car v) 470 0 0) v)) vertex)))
-  (post-redisplay main-window))
 
 (initialize-glut (program-arguments) #:window-size '(640 . 480) #:display-mode (display-mode rgb double))
 (set! main-window (make-window "rigs of rods"))
-(set-display-callback  on-display)
-(set-reshape-callback  on-reshape)
+(set-display-callback on-display)
+(set-reshape-callback on-reshape)
 (set-idle-callback     on-idle)
 (set-gl-clear-color 0 0 0 1)
 (set-gl-matrix-mode (matrix-mode modelview))
