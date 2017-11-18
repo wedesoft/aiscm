07a7285ebe035b53130474765358eb7de1ed7b30
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-18 21:39:57 +0000

    2D physics

--- etc/lander.scm
+++ etc/lander.scm
@@ -1,24 +1,36 @@
 #!/usr/bin/env guile
 !#
 ; http://twvideo01.ubm-us.net/o1/vault/gdc04/slides/using_verlet_integration.pdf
-(use-modules (glut) (gl) (gl low-level) (glu) (srfi srfi-1) (srfi srfi-26))
+(use-modules (oop goops) (glut) (gl) (gl low-level) (glu) (srfi srfi-1) (srfi srfi-26))
 
 (define dt 0.08)
 (define pi 3.141592653589793)
+(define g '(0 0.1 0))
+(define engine '(0 -0.3 0))
+(define nozzle -0.05)
+(define thrust 0)
+(define rcs 0)
 
-(define body '((-40 -20 1) ( 40 -20 1)
-               (-40 -20 1) (-40  20 1)
-               ( 40 -20 1) ( 40  20 1)
-               (-40  20 1) ( 40  20 1)))
+(define-method (+ (a <list>) (b <list>)) (map + a b))
+(define-method (- (a <list>) (b <list>)) (map - a b))
+(define-method (* (a <real>) (b <list>)) (map (cut * a <>) b))
+(define-method (* (a <list>) (b <real>)) (map (cut * <> b) a))
 
-(define ship '(320 240 0))
-(define ship_ (list (- 320 (* 5 dt)) 240 (/ (* 2 pi dt) 40)))
-(define ground 470)
+(define body '((-4 -2 1) ( 4 -2 1)
+               (-5 -2 1) (-4  2 1)
+               ( 4 -3 1) ( 4  2 1)
+               (-4  2 1) ( 4  2 1)))
 
-(define (matrix pose)
-  (let [(x (car pose))
-        (y (cadr pose))
-        (a (caddr pose))]
+(define position '(32 24 1))
+(define position_ '(32 24 1))
+(define angle 0)
+(define angle_ 0)
+(define ground 47.0)
+
+(define (matrix position angle)
+  (let [(x (car position))
+        (y (cadr position))
+        (a angle)]
     (list (list (cos a) (- (sin a)) x)
           (list (sin a)    (cos a)  y)
           (list       0          0  1))))
@@ -33,12 +45,25 @@
   (gl-viewport 0 0 width height)
   (set-gl-matrix-mode (matrix-mode projection))
   (gl-load-identity)
-  (gl-ortho 0 width height 0 -1 +1))
+  (gl-ortho 0 64 48 0 -1 +1))
+
+(define (on-key keycode x y)
+  (let [(c (integer->char keycode))]
+    (case c
+      ((#\w) (set! thrust (- 1 thrust)))
+      ((#\a) (set! rcs  1))
+      ((#\d) (set! rcs -1))
+      ((#\s) (set! rcs  0)))))
 
 (define (on-idle)
-  (let [ (update (map (lambda (v v_) (- (* 2 v) v_)) ship ship_))]
-    (set! ship_ ship)
-    (set! ship update)
+  (let* [(acceleration (+ g (* thrust (dot (matrix position angle) engine))))
+         (angular      (* rcs nozzle))
+         (position+    (+ (- (* 2 position) position_) (* acceleration dt dt)))
+         (angle+       (+ (- (* 2 angle) angle_) (* angular dt dt)))]
+    (set! position_ position)
+    (set! position position+)
+    (set! angle_ angle)
+    (set! angle angle+)
     (post-redisplay)))
 
 (define (gl-vertex-2d v) (gl-vertex (car v) (cadr v) 0))
@@ -47,7 +72,7 @@
   (gl-clear (clear-buffer-mask color-buffer))
   (gl-begin (begin-mode lines)
     (gl-color 0 1 0)
-    (for-each gl-vertex-2d (map (cut dot (matrix ship) <>) body))
+    (for-each gl-vertex-2d (map (cut dot (matrix position angle) <>) body))
     (gl-vertex-2d (list 0 ground)) (gl-vertex-2d (list 640 ground)))
   (swap-buffers))
 
@@ -55,9 +80,9 @@
 (set! main-window (make-window "rigs"))
 (set-display-callback on-display)
 (set-reshape-callback on-reshape)
-(set-idle-callback     on-idle)
+(set-idle-callback on-idle)
+(set-keyboard-callback on-key)
 (set-gl-clear-color 0 0 0 1)
 (set-gl-matrix-mode (matrix-mode modelview))
 (gl-load-identity)
 (glut-main-loop)
-
