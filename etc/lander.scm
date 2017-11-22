9b4173a07c3e496b6a6808b8564d6ba245f6a21e
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-22 22:09:46 +0000

    Reenable visualisation

--- etc/lander.scm
+++ etc/lander.scm
@@ -5,7 +5,9 @@
 ; http://www.cs.unc.edu/~lin/COMP768-F07/LEC/rbd2.pdf
 (use-modules (oop goops) (glut) (gl) (gl low-level) (glu) (srfi srfi-1) (srfi srfi-26) (ice-9 format))
 
+; use RK4 to get same solution regardless of time step
 (define dt 0.04)
+(define reps 5)
 (define scale (/ 1 dt))
 (define pi 3.141592653589793)
 (define g '(0 0.1 0))
@@ -32,11 +34,15 @@
 (define position '(32 24 1))
 (define position_ '(32 24 1))
 (define angle -0.3)
-(define angle_ -0.29)
+(define angle_ (+ -0.3 (* 0.05 dt)))
 (define ground 40.0)
 
-(define (cross r n)
-  (- (* (car r) (cadr n)) (* (cadr r) (car n))))
+(define (argop op fun lst)
+  (let* [(vals  (map fun lst))
+         (opval (apply op vals))]
+    (list-ref (reverse lst) (1- (length (member opval vals))))))
+(define (argmin fun lst) (argop min fun lst))
+(define (argmax fun lst) (argop max fun lst))
 
 (define (spin p dangle)
   (* dangle (list (- (cadr p)) (car p) 0)))
@@ -70,6 +76,7 @@
       ((#\s) (set! rcs  0)))))
 
 (define (on-idle)
+  (for-each (lambda _
   (let* [(acceleration (+ g (* thrust (dot (matrix position angle) engine))))
          (angular      (* rcs nozzle))
          (position+    (+ (- (* 2 position) position_) (* acceleration dt dt)))
@@ -79,20 +86,25 @@
     (set! angle_ angle)
     (set! angle angle+)
     (let* [(outer     (map (cut dot (matrix position angle) <>) body))
-           (collision (find (lambda (p) (>= (cadr p) ground)) outer))]
-      (if collision
+           (collision (argmax cadr outer))]
+      (if (>= (cadr collision) ground)
         (let* [(r    (- collision position))
                (vrel (cadr (+ (- position position_) (spin (- collision position) (- angle angle_)))))
                (j    (/ (* (- loss 2) vrel) (+ (/ 1 m) (* (car r) (car r) (/ 1 t)))))
+               (eps  (- (cadr collision) ground))
                (dv   (/ j m))
                (dw   (* (car r) (/ j t)))]
           (if (>= vrel 0)
             (begin
-              (format #t "vrel: ~a, dangle: ~a, j: ~a, dv: ~a, dw: ~a~&" vrel (- angle angle_) j dv dw)
-              (set! position_ (list (car position_) (- (cadr position_) dv)))
+              ;(format #t "vrel: ~a, dangle: ~a, j: ~a, dv: ~a, dw: ~a~&" vrel (- angle angle_) j dv dw)
+              (set! position_ (- position_ (list 0 dv)))
+              (set! position (- position (list 0 eps)))
+              (set! position_ (- position_ (list 0 eps)))
               (set! angle_ (- angle_ dw))
-              (format #t "vrel': ~a~&" (cadr (+ (- position position_) (spin (- collision position) (- angle angle_))))))))))
-    (post-redisplay)))
+              ;(format #t "vrel': ~a~&" (cadr (+ (- position position_) (spin (- collision position) (- angle angle_)))))
+              )))))))
+  (iota reps))
+  (post-redisplay))
 
 (define (gl-vertex-2d v) (gl-vertex (car v) (cadr v) 0))
 
