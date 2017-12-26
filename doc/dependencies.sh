71b8cddbfcdb0ae543ff650e667b26183e7ba87a
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-12-26 21:20:19 +0000

    Use Ubuntu Xenial dependency for Travis CI build

--- doc/dependencies.sh
+++ doc/dependencies.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -q --yes build-essential autoconf automake libtool
-sudo apt-get install -q --yes guile-2.2-dev linux-libc-dev gettext libmjpegtools-dev imagemagick libmagickcore-dev libpulse-dev libjpeg-dev libx11-dev libxext-dev libxv-dev libxmu-dev libxi-dev libxpm-dev libglu1-mesa-dev libgl1-mesa-dev libswscale-dev libavformat-dev libavcodec-dev libavutil-dev libavresample-dev pandoc
+sudo apt-get install -q --yes guile-2.0-dev linux-libc-dev gettext libmjpegtools-dev imagemagick libmagickcore-dev libpulse-dev libjpeg-dev libx11-dev libxext-dev libxv-dev libxmu-dev libxi-dev libxpm-dev libglu1-mesa-dev libgl1-mesa-dev libswscale-dev libavformat-dev libavcodec-dev libavutil-dev libavresample-dev pandoc
