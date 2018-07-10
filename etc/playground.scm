436f8f82021c98ab630751f546872b63f91e1a77
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-07-10 15:32:31 +0100

    Experimenting with lists

--- etc/playground.scm
+++ etc/playground.scm
@@ -1,11 +1,3 @@
 (use-modules (oop goops) (aiscm llvm) (aiscm util) (system foreign) (rnrs bytevectors) (aiscm basictype) (srfi srfi-1) (srfi srfi-26))
 
-
-(define m (make (multiarray <byte> 1) #:shape '(6) #:memory (bytevector->pointer #vu8(2 3 5 7 11 13))))
-
-((llvm-typed (list (llvmarray <sint> 1)) (lambda (m) (fetch (memory m)))) m)
-
-((llvm-typed (list (llvmlist <int> 2)) (lambda (l) (llvmlist (get l 0)))) '(6 4))
-
-((llvm-typed (list <int> <int>) (lambda (x y) (llvmlist x y))) 2 3)
-
+((llvm-typed (list (llvmlist <int> 3)) identity) '(2 3 5))
