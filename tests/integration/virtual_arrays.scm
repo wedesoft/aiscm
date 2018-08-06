4b63f6c6ed3ba3de114a0b4dd6a5617c0b737502
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-08-06 17:21:04 +0100

    Minor changes

--- tests/integration/virtual_arrays.scm
+++ tests/integration/virtual_arrays.scm
@@ -1,6 +1,5 @@
-(use-modules (oop goops) (aiscm asm) (aiscm jit) (aiscm int) (aiscm rgb) (aiscm sequence))
-(define ctx (make <context>))
-(define f (jit ctx (list (rgb <int>) (sequence <int>)) +))
-(f (rgb 1 2 3) (seq <int> 4 5 6))
+(use-modules (oop goops) (aiscm core))
+(define f (jit (list (rgb <int>) (multiarray <int> 1)) +))
+(f (rgb 1 2 3) (arr <int> 4 5 6))
 ; #<sequence<rgb<int<32,signed>>>>:
 ; ((rgb 5 6 7) (rgb 6 7 8) (rgb 7 8 9))
