7cc70af1fded6928f52a64c1d48b5cab8480c1db
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-09-07 13:08:32 +0100

    Element-wise loop using phi values

--- aiscm/core.scm
+++ aiscm/core.scm
@@ -1373,18 +1373,38 @@
   "Elementwise array operation with arbitrary arity"
   (if (zero? (dimensions result))
     (store (memory result) (apply delegate args))
-    (let [(q (map (lambda (arg) (if (is-a? arg <llvmarray<>>) (typed-alloca (pointer (typecode arg))) #f)) args))]
-      (jit-let [(pend (+ (memory result) (* (llvm-last (shape result)) (llvm-last (strides result)))))]
-        (apply llvm-begin (append-map (lambda (ptr arg) (if ptr (list (store ptr (memory arg))) '())) q args))
-        (jit-for p (memory result) pend (llvm-last (strides result))
-          (apply elementwise-loop delegate
-                                  (project (rebase result p))
-                                  (map (lambda (ptr arg) (if ptr (fetch (project (rebase arg (fetch ptr)))) arg)) q args))
-          (apply llvm-begin
-            (append-map
-              (lambda (ptr arg)
-                (if ptr (list (store ptr (+ (fetch ptr) (llvm-last (strides arg))))) '()))
-              q args)))))))
+    (let [(start (make-basic-block "start"))
+          (for    (make-basic-block "for"))
+          (body   (make-basic-block "body"))
+          (finish (make-basic-block "finish"))
+          (end    (make-basic-block "end"))]
+      (llvm-begin
+        (build-branch start)
+        (position-builder-at-end start)
+        (jit-let [(pend (+ (memory result) (* (llvm-last (shape result)) (llvm-last (strides result)))))]
+          (build-branch for)
+          (position-builder-at-end for)
+          (jit-let [(p (build-phi (pointer (typecode result))))]
+            (let [(q (map (lambda (arg) (if (is-a? arg <llvmarray<>>) (build-phi (pointer (typecode arg))) #f)) args))]
+              (llvm-begin
+                (add-incoming p start (memory result))
+                (apply llvm-begin (append-map (lambda (ptr arg) (if ptr (list (add-incoming ptr start (memory arg))) '())) q args))
+                (build-cond-branch (ne p pend) body end)
+                (position-builder-at-end body)
+                (apply elementwise-loop
+                       delegate
+                       (project (rebase result p))
+                       (map (lambda (ptr arg) (if ptr (fetch (project (rebase arg ptr))) arg)) q args))
+                (build-branch finish)
+                (position-builder-at-end finish)
+                (add-incoming p finish (+ p (llvm-last (strides result))))
+                (apply llvm-begin
+                  (append-map
+                    (lambda (ptr arg) (if ptr (list (add-incoming ptr finish (+ ptr (llvm-last (strides arg))))) '()))
+                    q
+                    args))
+                (build-branch for)
+                (position-builder-at-end end)))))))))
 
 (define (compute-strides typecode shape)
   "Compile code for computing strides"
