b639bee4f688c4f74692f3609f4fff990b38a2da
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-09-20 22:01:46 +0100

    Prototyping tensor operations

--- etc/playground.scm
+++ etc/playground.scm
@@ -1,3 +1,16 @@
 (use-modules (oop goops) (aiscm util) (system foreign) (rnrs bytevectors) (aiscm core) (aiscm image) (aiscm magick) (aiscm xorg) (aiscm v4l2) (srfi srfi-1) (srfi srfi-26))
 
-(define-tensor (dot arr w h) (tensor w h (get arr i j)))
+(define-method (native-type2 value) (native-type value))
+(define-method (native-type2 (value <integer>)) <int>)
+
+(define-syntax-rule (define-tensor (name args ...) expression)
+  (define-method (name args ...)
+    (let [(fun (jit (map native-type2 (list args ...)) (lambda (args ...) expression)))]
+      (add-method! name
+                   (make <method> #:specializers (map class-of (list args ...))
+                                  #:procedure fun))
+      (name args ...))))
+
+(define-tensor (test x) (+ x x))
+
+;(define-tensor (name arr size) (tensor [size] i (get arr i)))
