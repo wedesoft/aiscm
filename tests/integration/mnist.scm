e3807c22320a29a960fa7e30cc21ce0b6b073ea7
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-12-07 21:59:29 +0000

    Add checks to MNIST example

--- tests/integration/mnist.scm
+++ tests/integration/mnist.scm
@@ -1,18 +1,21 @@
 (use-modules (oop goops) (ice-9 binary-ports) (rnrs bytevectors) (aiscm core) (system foreign) (aiscm xorg) (ice-9 format))
 ; http://yann.lecun.com/exdb/mnist/
-(define f (open-file "train-labels-idx1-ubyte" "rb"))
-(define magic (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
-(define n (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
-(define bv (get-bytevector-n f n))
-(define labels (make (multiarray <ubyte> 1) #:memory (bytevector->pointer bv) #:shape (list n)))
-
 (define f (open-file "train-images-idx3-ubyte" "rb"))
 (define magic (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
+(if (not (eqv? magic 2051)) (error "Images file has wrong magic number"))
 (define n (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
 (define h (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
 (define w (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
 (define bv (get-bytevector-n f (* n h w)))
 (define images (make (multiarray <ubyte> 3) #:memory (bytevector->pointer bv) #:shape (list n h w)))
+
+(define f (open-file "train-labels-idx1-ubyte" "rb"))
+(define magic (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
+(if (not (eqv? magic 2049)) (error "Label file has wrong magic number"))
+(define n2 (bytevector-u32-ref (get-bytevector-n f 4) 0 (endianness big)))
+(if (not (eqv? n n2)) (error "Number of labels does not match number of images"))
+(define bv (get-bytevector-n f n))
+(define labels (make (multiarray <ubyte> 1) #:memory (bytevector->pointer bv) #:shape (list n)))
 
 (define i -1)
 (show
