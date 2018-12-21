c4d4bb8841cdd0ca219be5f3b96136bed488d4e7
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-12-21 21:36:54 +0000

    Load audio training data

--- tests/integration/voice-train.scm
+++ tests/integration/voice-train.scm
@@ -1,17 +1,13 @@
-(use-modules (oop goops) (ice-9 textual-ports) (ice-9 format) (aiscm core) (aiscm ffmpeg) (aiscm tensorflow))
+(use-modules (oop goops) (ice-9 textual-ports) (ice-9 format) (aiscm core) (aiscm ffmpeg) (aiscm tensorflow) (aiscm util))
 
-(define audio (open-ffmpeg-input "voice-commands.mp3"))
 (define words (list "stop" "go" "left" "right"))
-(define t 0.0)
-(define word "stop")
-(define next-t 0.0)
-(define next-word "stop")
 (define n-hidden 64)
+(define m 20)
 
-(define x (tf-placeholder #:dtype <sint> #:shape '(1 512) #:name "x"))
-(define h (tf-placeholder #:dtype <double> #:shape (list 1 n-hidden) #:name "h"))
-(define c (tf-placeholder #:dtype <double> #:shape (list 1 n-hidden) #:name "c"))
-(define fourier (tf-cast (tf-rfft (tf-cast x #:DstT <float>) (arr <int> 512)) #:DstT <double>))
+(define x (tf-placeholder #:dtype <sint> #:shape '(-1 512) #:name "x"))
+(define y (tf-placeholder #:dtype <ubyte> #:shape '(-1 4) #:name "y"))
+(define h0 (tf-placeholder #:dtype <double> #:shape (list 1 n-hidden) #:name "h"))
+(define c0 (tf-placeholder #:dtype <double> #:shape (list 1 n-hidden) #:name "c"))
 (define wf (tf-variable #:dtype <double> #:shape (list 257 n-hidden)))
 (define wi (tf-variable #:dtype <double> #:shape (list 257 n-hidden)))
 (define wo (tf-variable #:dtype <double> #:shape (list 257 n-hidden)))
@@ -24,6 +20,11 @@
 (define bi (tf-variable #:dtype <double> #:shape (list 1 n-hidden)))
 (define bo (tf-variable #:dtype <double> #:shape (list 1 n-hidden)))
 (define bc (tf-variable #:dtype <double> #:shape (list 1 n-hidden)))
+(define wy (tf-variable #:dtype <double> #:shape (list n-hidden 4)))
+(define by (tf-variable #:dtype <double> #:shape (list 1 4)))
+
+(define (fourier x)
+  (tf-cast (tf-reshape (tf-rfft (tf-cast x #:DstT <float>) (arr <int> 512)) (arr <int> 1 257)) #:DstT <double>))
 
 (define (lstm fourier h c)
   (let* [(f (tf-add-n (list (tf-mat-mul fourier wf) (tf-mat-mul h uf) bf)))
@@ -34,21 +35,63 @@
          (h_ (tf-mul o (tf-tanh c_)))]
     (list h_ c_)))
 
-(lstm fourier h c)
+(define (output h) (tf-add (tf-mat-mul h wy) by))
 
+(define (nth x i) (tf-nth-element (tf-transpose x (arr <int> 1 0)) i))
+
+(define (invert x) (tf-sub 1.0 x))
+
+(define h_ h0)
+(define c_ c0)
+
+(define cost 0.0)
+
+(for-each
+  (lambda (i)
+    (let* [(memory (lstm (fourier (nth x i)) h_ c_))
+           (h      (output (car memory)))]
+      (set! h_ (car memory))
+      (set! c_ (cadr memory))
+      (set! cost (tf-add cost (tf-mean (tf-add (tf-mul (tf-cast (nth y i) #:DstT <double>) (tf-log h))
+                                               (tf-mul (invert (tf-cast (nth y i) #:DstT <double>)) (tf-log (invert h))))
+                                       (arr <int> 0))))))
+  (iota m))
+
+(define s (make-session))
+
+(define vars (list wf wi wo wc uf ui uo uc bf bi bo bc wy by))
+
+(define t 0.0)
+(define word "stop")
+(define next-t 0.0)
+(define next-word "stop")
+
+(define audio (open-ffmpeg-input "voice-commands.mp3"))
+(define l -1)
+(while (read-audio audio 512) (set! l (1+ l)))
+
+(define in (make (multiarray <sint> 2) #:shape (list l 512)))
+(define out (make (multiarray <ubyte> 2) #:shape (list l 4)))
+
+(define i 0)
+(define audio (open-ffmpeg-input "voice-commands.mp3"))
 (call-with-input-file "voice-commands.csv"
   (lambda (port)
     (get-line port)
     (while #t
-      (let [(line (get-line port))]
-        (if (eof-object? line) (break))
-        (let [(columns (string-split line #\,))]
-          (set! word next-word)
-          (set! next-t (string->number (car columns)))
-          (set! next-word (cadr columns))))
-      (while (< t next-t)
-        (let [(samples (read-audio audio 512))]
-          (if (or (not samples) (< (car (shape samples)) 512))
-            (break))
-          (format #t "current output: ~a ~a~&" word (typecode samples))
-          (set! t (+ t (/ 512 (rate audio)))))))))
+      (let [(line (get-line port))
+            (outer-break break)]
+        (set! word next-word)
+        (if (eof-object? line)
+          (set! next-t 1e+99)
+          (let [(columns (string-split line #\,))]
+            (set! next-t (string->number (car columns)))
+            (set! next-word (cadr columns))))
+        (while (< t next-t)
+          (let [(samples (read-audio audio 512))]
+            (if (>= i l) (outer-break))
+            (set in i (reshape (to-array samples) '(512)))
+            (set out i 0)
+            (set out (index-of word words) i 1)
+            (set! i (1+ i))
+            (set! t (+ t (/ 512 (rate audio))))))))))
