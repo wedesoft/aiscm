a9408e4ce95d3d90f6db818b6b86ac15018d2a30
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-01-02 22:49:33 +0000

    Changed program for recording audio

--- tests/integration/voice-record.scm
+++ tests/integration/voice-record.scm
@@ -1,33 +1,33 @@
 (use-modules (oop goops)
              (srfi srfi-18)
-             (ice-9 readline)
              (ice-9 format)
-             (ice-9 threads)
              (aiscm core)
              (aiscm ffmpeg)
              (aiscm pulse)
              (aiscm util))
 
 (define words (list "stop" "go" "left" "right"))
-(define output (open-ffmpeg-output "voice-commands.mp3" #:rate 11025 #:typecode <sint> #:channels 1 #:audio-bit-rate 80000))
-(define record (make <pulse-record> #:typecode <sint> #:channels 1 #:rate 11025))
-(define time (clock))
-(define q #f)
-(define thread
-  (call-with-new-thread
-    (lambda ()
-      (while (not q)
-        (let [(samples (read-audio record 4410))]
-          (write-audio samples output))))))
-(call-with-output-file "voice-commands.csv"
-  (lambda (port)
-    (format port "time,word~&")
-    (while #t
-      (let [(choice (list-ref words (random (length words))))]
-        (format #t "Say \"~a\" and press enter (x and enter to exit)." choice)
-        (if (not (equal? (readline) ""))
-          (break))
-        (format port "~a,~a~&" (elapsed time) choice)))))
-(set! q #t)
-(join-thread thread)
+(define rate 11025)
+(define chunk 512)
+(define rising 2000)
+(define falling 1000)
+(define output (open-ffmpeg-output "voice-commands.mp3" #:rate rate #:typecode <sint> #:channels 1 #:audio-bit-rate 80000))
+(define csv (open-file "voice-commands.csv" "wl"))
+(define record (make <pulse-record> #:typecode <sint> #:channels 1 #:rate rate))
+(format csv "time,word~&")
+(define time 0.0)
+(define choice "")
+(define status 'on)
+(while #t
+  (let* [(samples (read-audio record chunk))
+        (loudness (sqrt (/ (sum (* (to-type <int> samples) samples)) chunk)))]
+    (if (and (eq? status 'off) (> loudness rising)) (set! status 'on))
+    (if (and (eq? status 'on) (< loudness falling))
+      (begin
+        (set! status 'off)
+        (set! choice (list-ref words (random (length words))))
+        (format #t "Say \"~a\"~&" choice)))
+    (write-audio samples output)
+    (format csv "~a,~a~&" time (if (eq? status 'off) "" choice))
+    (set! time (+ time (/ chunk rate)))))
 (destroy output)
