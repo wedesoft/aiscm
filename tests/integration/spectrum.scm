a6dac263535b5cd06d20edee74808513f439a095
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-01-16 21:45:23 +0000

    Update spectrum example

--- tests/integration/spectrum.scm
+++ tests/integration/spectrum.scm
@@ -1,15 +1,17 @@
 (use-modules (oop goops) (aiscm core) (aiscm pulse) (aiscm tensorflow) (aiscm xorg))
 (define s (make-session))
-(define sample (tf-placeholder #:dtype <float> #:shape '(1 512)))
-(define vec (tf-reshape sample (arr <int> 512)))
-(define fourier (tf-rfft vec (arr <int> 512)))
+(define sample (tf-placeholder #:dtype <float> #:shape '(1 256)))
+(define vec (tf-reshape sample (arr <int> 256)))
+(define fourier (tf-rfft vec (arr <int> 256)))
 (define spectrum (tf-real (tf-mul fourier (tf-conj fourier))))
 (define record (make <pulse-record> #:typecode <float> #:channels 1 #:rate 11025))
+(define ramp (/ (indices 256) 128.0))
+(define window (to-type <float> (minor ramp (- 2.0 ramp))))
 (define x 0)
-(define img (fill <ubyte> '(257 512) 0))
+(define img (fill <ubyte> '(129 256) 0))
 (show
   (lambda _
-    (let [(spec (run s (list (cons sample (read-audio record 512))) spectrum))]
-      (set img x '(0 257) spec)
-      (set! x (modulo (1+ x) 512))
+    (let [(spec (run s (list (cons sample (* window (read-audio record 256)))) spectrum))]
+      (set img x '(0 129) (minor 255 spec))
+      (set! x (modulo (1+ x) 256))
       img)))
