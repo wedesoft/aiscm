f7148d2905d19b5e6437e8ee88627d019f6f27ea
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-01-18 21:23:00 +0000

    minor changes to spectrum example

--- tests/integration/spectrum.scm
+++ tests/integration/spectrum.scm
@@ -1,17 +1,19 @@
 (use-modules (oop goops) (aiscm core) (aiscm pulse) (aiscm tensorflow) (aiscm xorg))
 (define s (make-session))
-(define sample (tf-placeholder #:dtype <float> #:shape '(1 256)))
-(define vec (tf-reshape sample (arr <int> 256)))
-(define fourier (tf-rfft vec (arr <int> 256)))
+(define w 400)
+(define h 512)
+(define sample (tf-placeholder #:dtype <float> #:shape (list 1 h)))
+(define vec (tf-reshape sample (to-array <int> (list h))))
+(define fourier (tf-rfft vec (to-array <int> (list h))))
 (define spectrum (tf-mul (tf-log (tf-real (tf-mul fourier (tf-conj fourier)))) (tf-cast 32.0 #:DstT <float>)))
-(define record (make <pulse-record> #:typecode <float> #:channels 1 #:rate 11025))
-(define ramp (/ (indices 256) 128.0))
+(define record (make <pulse-record> #:typecode <float> #:channels 1 #:rate 16000))
+(define ramp (* (indices h) (/ 2.0 h)))
 (define window (to-type <float> (minor ramp (- 2.0 ramp))))
 (define x 0)
-(define img (fill <ubyte> '(129 256) 0))
+(define img (fill <ubyte> (list (1+ (/ h 2)) w) 0))
 (show
   (lambda _
-    (let [(spec (run s (list (cons sample (* window (read-audio record 256)))) spectrum))]
-      (set img x '(0 129) (major (minor 255 spec) 0))
-      (set! x (modulo (1+ x) 256))
+    (let [(spec (run s (list (cons sample (* window (read-audio record h)))) spectrum))]
+      (set img x (list 0 (1+ (/ h 2))) (major (minor 255 spec) 0))
+      (set! x (modulo (1+ x) w))
       img)))
