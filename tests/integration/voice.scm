1bfe14de2481da39fe60f050b07b640bc8cd045e
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-01-20 20:59:43 +0000

    Read in audio samples

--- tests/integration/voice.scm
+++ tests/integration/voice.scm
@@ -1,10 +1,11 @@
 (use-modules (ice-9 ftw)
              (ice-9 format)
+             (aiscm core)
              (aiscm ffmpeg))
 
 
+(define samples '())
 (ftw "speech/go"
   (lambda (filename statinfo flag)
-    (if (eq? flag 'regular)
-      (format #t "~a~&" filename))
+    (if (eq? flag 'regular) (set! samples (cons (read-audio (open-ffmpeg-input filename) 64000) samples)))
     #t))
