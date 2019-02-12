59d000124b0b144de8b6e6f9c9b86ce37962496b
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-02-12 21:52:51 +0000

    Trying to run Aruco detector

--- etc/cc.cpp
+++ etc/cc.cpp
@@ -1,12 +1,17 @@
+#include <iostream>
 #include <opencv2/aruco/charuco.hpp>
 #include <opencv2/imgcodecs.hpp>
 
+using namespace std;
+
 int main(void)
 {
-  cv::Mat m(700, 500, CV_8UC1);
-  cv::Ptr< cv::aruco::Dictionary > dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-  cv::Ptr< cv::aruco::CharucoBoard > charuco = cv::aruco::CharucoBoard::create(7, 5, 100, 50, dict);
-  charuco->draw(cv::Size(700, 500), m, 0, 1);
-  cv::imwrite("board.png", m);
+  cv::Mat m(cv::imread("choriginal.png", cv::IMREAD_GRAYSCALE));
+  cv::Ptr<cv::aruco::Dictionary> dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+  // cv::Ptr<cv::aruco::CharucoBoard> charuco = cv::aruco::CharucoBoard::create(5, 7, 0.04, 0.02, dict);
+  std::vector<int> markerIds;
+  std::vector<std::vector<cv::Point2f>> markerCorners;
+  cv::aruco::detectMarkers(m, dict, markerCorners, markerIds);
+  cout << m.cols << "x" << m.rows << ": " << markerIds.size() << endl;
   return 0;
 }
