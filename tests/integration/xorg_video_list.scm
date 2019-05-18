ca7fbdb7372e37753f1b15385b4ca79ed5b251cb
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-05-18 21:29:29 +0100

    Updated X.Org video list example

--- tests/integration/xorg_video_list.scm
+++ tests/integration/xorg_video_list.scm
@@ -1,4 +1,4 @@
-(use-modules (oop goops) (aiscm v4l2) (aiscm core) (aiscm xorg))
+(use-modules (oop goops) (aiscm v4l2) (aiscm core) (aiscm image) (aiscm xorg))
 (define v (make <v4l2>))
-(show (lambda _ (let [(img (to-array (read-image v)))] (list (red img) (green img) (blue img)))))
+(show (lambda _ (let [(img (from-image (read-image v)))] (list (red img) (green img) (blue img)))))
 (destroy v)
