e546c22a28d6a0d9a14ce92cf90b247b340a4616
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-02-05 20:26:44 +0000

    Readd test-driver.scm

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 git log > ChangeLog
-mkdir -p build-aux
-touch build-aux/config.rpath
-autoreconf -i
+# mkdir -p build-aux
+# touch build-aux/config.rpath
+aclocal -I m4
+autoheader
+libtoolize --force
+automake -a --foreign
+autoconf
